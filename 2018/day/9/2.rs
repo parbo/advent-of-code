c202968d8044c0114ffe6e2c9f30b4c2a49326d5
Pär Bohrarper <par.bohrarper@gmail.com>
2018-12-09 15:16:22 +0100

    some cleanup

--- 2018/day/9/2.rs
+++ 2018/day/9/2.rs
@@ -7,29 +7,25 @@ use std::env;
 fn solve(players: i64, marbles: i64) -> i64 {
     let mut circle = LinkedList::new();
     circle.push_front(0);
-    let mut player = 1;
     let mut cursor = circle.cursor();
     let mut score = HashMap::new();
     for marble in 1..(marbles+1) {
         if marble % 23 == 0 {
             for _ in 0..7 {
-                if let Some(_) = cursor.prev() {
-                } else {
+                if let None = cursor.prev() {
                     cursor.prev();
                 }
             }
             let removed = cursor.remove().unwrap();
-            *score.entry(player).or_insert(0) += removed + marble;
+            *score.entry(marble % players).or_insert(0) += removed + marble;
         } else {
             for _ in 0..2 {
-                if let Some(_) = cursor.next() {
-                } else {
+                if let None = cursor.next() {
                     cursor.next();
                 }
             }
             cursor.insert(marble);
         }
-        player = (player + 1) % players;
     }
     return *score.iter().map(|a| a.1).max().unwrap();
 }

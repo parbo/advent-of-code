154cf8af680cc6a16fa126177b55a2be62d4c702
Pär Bohrarper <par.bohrarper@gmail.com>
2018-12-13 07:26:45 +0100

    day 13 wip

---
+++ 2018/day/13/1.rs
@@ -0,0 +1,58 @@
+use std::env;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::iter::*;
+use std::path::Path;
+
+fn draw_grid(grid: &Vec<Vec<char>>) {
+    for row in grid {
+        for col in row {
+            print!("{}", col);
+        }
+        println!("");
+    }
+}
+
+#[derive(Debug)]
+struct Car {
+    dir: char,
+    pos: (usize, usize),
+    next_dir: char
+}
+
+fn solve(path: &Path) -> (i64, i64) {
+    let input = File::open(path).unwrap();
+    let buffered = BufReader::new(input);
+    let lines : Vec<String> = buffered.lines().filter_map(Result::ok).collect();
+    let mut grid = vec![];
+    let mut max_w = 0;
+    let mut cars = vec![];
+    let mut m = HashMap::new();
+    m.insert('<', '-');
+    m.insert('>', '-');
+    m.insert('v', '|');
+    m.insert('^', '|');
+    for (row, line) in lines.iter().enumerate() {
+        let row_cars : Vec<Car>= line.chars().enumerate().filter(|(_, c)| ['<', '>', 'v', '^'].contains(&c)).map(|(col, c)| Car {dir: c, pos: (col, row), next_dir: '<' }).collect();
+        cars.extend(row_cars);
+        let v : Vec<char> = line.chars().map(|c| *m.get(&c).unwrap_or(&c)).collect();
+        max_w = std::cmp::max(v.len(), max_w);
+        grid.push(v);
+    }
+    for row in &mut grid {
+        row.resize(max_w, ' ');
+    }
+    println!("{:?}", cars);
+//    draw_grid(&grid);
+    return (-1, -1);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let result = solve(Path::new(&filename));
+    println!("{:?}", result);
+}

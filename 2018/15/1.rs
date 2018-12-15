fce47359a960276562619505f542d6dd9f555b4a
Pär Bohrarper <par.bohrarper@gmail.com>
2018-12-15 19:55:39 +0100

    all tests work

--- 2018/15/1.rs
+++ 2018/15/1.rs
@@ -15,7 +15,7 @@ pub trait Neighbours {
 #[derive(Copy, Clone, Eq, PartialEq)]
 struct State {
     cost: usize,
-    position: (usize, usize)  // note: this is y, x for comparison reasons
+    position: (usize, usize)  // y, x
 }
 
 // The priority queue depends on `Ord`.
@@ -27,7 +27,7 @@ impl Ord for State {
         // In case of a tie we compare positions - this step is necessary
         // to make implementations of `PartialEq` and `Ord` consistent.
         other.cost.cmp(&self.cost)
-            .then_with(|| self.position.cmp(&other.position))
+            .then_with(|| other.position.cmp(&self.position))
     }
 }
 
@@ -61,7 +61,7 @@ fn shortest_path(n: &Neighbours, start: (usize, usize), goal: (usize, usize)) ->
             let mut path = vec![goal];
             let mut current = goal;
             while let Some(pos) = came_from.get(&current) {
-                path.push(*pos);
+                path.insert(0, *pos);
                 current = *pos;
             }
             return Some(path);
@@ -72,11 +72,10 @@ fn shortest_path(n: &Neighbours, start: (usize, usize), goal: (usize, usize)) ->
             continue;
         }
 
-        println!("pos: {:?}", position);
-
         // For each node we can reach, see if we can find a way with
         // a lower cost going through this node
         let neighbours = n.neighbours(position);
+//        println!("neigh: {:?} => {:?}", position, neighbours);
         for neighbour_position in &neighbours {
             let next = State { cost: cost + 1, position: *neighbour_position };
 
@@ -97,16 +96,22 @@ fn shortest_path(n: &Neighbours, start: (usize, usize), goal: (usize, usize)) ->
     None
 }
 
-#[derive(Copy, Clone, Eq, PartialEq)]
-struct Stats {
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum FighterKind {
+    Elf,
+    Goblin
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct Fighter {
+    kind: FighterKind,
     attack: i64,
     hp: i64
 }
 
-#[derive(Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 enum Entity {
-    Elf(Stats),
-    Goblin(Stats),
+    Being(Fighter),
     Wall,
     Floor
 }
@@ -117,28 +122,204 @@ struct Map {
 
 impl Neighbours for Map {
     fn neighbours(&self, pos: (usize, usize)) -> Vec<(usize, usize)> {
+        self.neighbours(pos, |p| self.map[p.0][p.1] == Entity::Floor)
+    }
+}
+
+impl Map {
+    fn neighbours(&self, pos: (usize, usize), filter: impl Fn((usize, usize)) -> bool) -> Vec<(usize, usize)> {
         let mut n = vec![];
         let y = pos.0 as i64;
         let x = pos.1 as i64;
         let w = self.map[0].len() as i64;
         let h = self.map.len() as i64;
-        for (ny, nx) in &[(y - 1, x -1),
-                          (y - 1, x),
-                          (y - 1, x + 1),
+        for (ny, nx) in &[(y - 1, x),
                           (y, x - 1),
                           (y, x + 1),
-                          (y + 1, x - 1),
-                          (y + 1, x),
-                          (y + 1, x + 1)] {
+                          (y + 1, x)] {
             if *nx > w || *ny > h || *ny < 0 || *nx < 0 {
                 continue;
             }
-            if self.map[*ny as usize][*nx as usize] != Entity::Floor {
-                continue;
+            let p = (*ny as usize, *nx as usize);
+            if !filter(p) {
+                continue
             }
             n.push((*ny as usize, *nx as usize));
         }
         n
+    }
+
+    fn entity(&self, pos: (usize, usize)) -> Entity {
+        self.map[pos.0][pos.1]
+    }
+
+    fn enemies_in_range(&self, pos: (usize, usize)) -> Vec<(usize, usize)> {
+        let mut enemies = vec![];
+        for npos in self.neighbours(pos, |_| true) {
+            if let Entity::Being(a) = self.entity(pos) {
+                if let Entity::Being(b) = self.entity(npos) {
+                    if a.kind != b.kind {
+                        enemies.push(npos);
+                    }
+                }
+            }
+        }
+        enemies
+    }
+
+    fn enemies(&self, pos: (usize, usize)) -> Vec<(usize, usize)> {
+        let mut enemies = vec![];
+        for (y, row) in self.map.iter().enumerate() {
+            for (x, entity) in row.iter().enumerate() {
+                if pos != (y, x) {
+                    if let Entity::Being(a) = self.entity(pos) {
+                        if let Entity::Being(b) = entity {
+                            if a.kind != b.kind {
+                                enemies.push((y, x));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        enemies
+    }
+
+    fn fighters(&self) -> Vec<(usize, usize)> {
+        let mut fighters = vec![];
+        for (y, row) in self.map.iter().enumerate() {
+            for (x, entity) in row.iter().enumerate() {
+                if let Entity::Being(_) = entity {
+                    fighters.push((y, x));
+                }
+            }
+        }
+        fighters
+    }
+
+    fn round(&mut self) -> bool {
+        let fighters = self.fighters();
+        for fa in &fighters {
+            if self.entity(*fa) == Entity::Floor {
+                // Already dead
+                continue;
+            }
+            let mut fighter = *fa;
+            // Do i have an enemy in range?
+            let mut enemies_to_fight = self.enemies_in_range(fighter);
+            if enemies_to_fight.len() == 0 {
+                // Find my enemies
+                let enemies = self.enemies(fighter);
+                if enemies.len() == 0 {
+                    // Combat is over
+                    return true;
+                }
+                // Find the closest enemy
+                let mut shortest = std::usize::MAX;
+                let mut shortest_steps = vec![];
+                for fb in &enemies {
+                    for adj in self.neighbours(*fb, |c| self.map[c.0][c.1] == Entity::Floor) {
+                        if let Some(p) = shortest_path(self, *fa, adj) {
+                            if p.len() < shortest {
+                                shortest = p.len();
+                                shortest_steps = vec![p];
+                            } else if p.len() == shortest {
+                                shortest_steps.push(p);
+                            }
+                        }
+                    }
+                }
+                if shortest_steps.len() > 0 {
+                    shortest_steps.sort();
+                    // Move
+                    let (y, x) = *fa;
+                    let (ny, nx) = shortest_steps[0][1];
+                    assert!(self.map[ny][nx] == Entity::Floor);
+                    self.map[ny][nx] = self.map[y][x];
+                    self.map[y][x] = Entity::Floor;
+                    fighter = (ny, nx);
+                }
+                // After moving, we might have some in range enemies
+                enemies_to_fight = self.enemies_in_range(fighter);
+            }
+            // Fight!
+            if let Entity::Being(_) = self.entity(fighter) {
+                // Find the weakest enemy
+                let mut min_hp = std::i64::MAX;
+                let mut min_enemy = None;
+                for close_enemy in enemies_to_fight {
+                    let entity = self.entity(close_enemy);
+                    match entity {
+                        Entity::Being(x) => {
+                            if x.hp < min_hp {
+                                min_hp = x.hp;
+                                min_enemy = Some(close_enemy);
+                            }
+                        },
+                        _ => panic!()
+                    }
+                }
+                if let Some(enemy) = min_enemy {
+                    let (y, x) = fighter;
+                    if let Entity::Being(attacker) = self.map[y][x] {
+                        let (yy, xx) = enemy;
+                        let mut dead = false;
+                        if let Entity::Being(attackee) = &mut self.map[yy][xx] {
+                            if attackee.hp >= attacker.attack {
+                                attackee.hp -= attacker.attack;
+                            } else {
+                                dead = true;
+                            }
+                        }
+                        if dead {
+                            // die!
+                            self.map[yy][xx] = Entity::Floor;
+                        }
+                    }
+                }
+            }
+        }
+        false
+    }
+
+    fn draw(&self) {
+        for row in &self.map {
+            let mut entities : Vec<Entity> = vec![];
+            for col in row {
+                let s = match col {
+                    Entity::Being(x) => {
+                        entities.push(*col);
+                        match x.kind {
+                            FighterKind::Elf => "E",
+                            FighterKind::Goblin => "G"
+                        }
+                    },
+                    Entity::Wall => "#",
+                    Entity::Floor => "."
+                };
+                print!("{}", s);
+            }
+            if entities.len() > 0 {
+                for e in entities {
+                    match e {
+                        Entity::Being(b) => {
+                            match b.kind {
+                                FighterKind::Elf => {
+                                    print!(" E({})", b.hp)
+                                },
+                                FighterKind::Goblin => {
+                                    print!(" G({})", b.hp)
+                                }
+                            }
+                        },
+                        _ => {}
+                    }
+                }
+                println!("");
+            } else {
+                println!("");
+            }
+        }
     }
 }
 
@@ -163,18 +344,36 @@ fn solve(path: &Path) {
             let entity = match col {
                 '#' => Entity::Wall,
                 '.' => Entity::Floor,
-                'E' => Entity::Elf(Stats { attack: 3, hp: 200 }),
-                'G' => Entity::Goblin(Stats { attack: 3, hp: 200 }),
+                'E' => Entity::Being(Fighter { kind: FighterKind::Elf, attack: 3, hp: 200 }),
+                'G' => Entity::Being(Fighter { kind: FighterKind::Goblin, attack: 3, hp: 200 }),
                 _ => panic!()
             };
             map_row.push(entity);
         }
         m.push(map_row);
     }
-    let map = Map { map: m };
+    let mut map = Map { map: m };
 
-    let path = shortest_path(&map, (13, 22), (16, 23));
-    println!("{:?}", path);
+    let mut rounds = 0;
+    map.draw();
+    while !map.round() {
+        rounds += 1;
+        println!("After {} rounds:", rounds);
+        map.draw();
+    }
+    map.draw();
+
+    let mut sum = 0;
+    for row in &map.map {
+        for col in row {
+            if let Entity::Being(x) = col {
+                println!("{:?}", x);
+                sum += x.hp;
+            }
+        }
+    }
+
+    println!("{}, {}, {}", rounds, sum, rounds * sum);
 }
 
 fn main() {

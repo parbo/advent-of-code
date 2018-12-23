161523fcada4aa31c90b9fefc3c4c6fa334c6e7d
Pär Bohrarper <par.bohrarper@gmail.com>
2018-12-23 14:07:40 +0100

    wip

--- 2018/23/nano.rs
+++ 2018/23/nano.rs
@@ -1,7 +1,4 @@
 use std::env;
-use std::collections::HashSet;
-use std::collections::HashMap;
-use std::collections::VecDeque;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
@@ -37,57 +34,7 @@ fn manhattan(a: &Bot, b: &Bot) -> i64 {
     (a.x - b.x).abs() + (a.y - b.y).abs() + (a.z - b.z).abs()
 }
 
-fn dist(a: &Bot) -> i64 {
-    (a.x - 0).abs() + (a.y - 0).abs() + (a.z - 0).abs()
-}
-
-fn overlapping(bots: &Vec<Bot>) -> Vec<Bot> {
-    let mut overlaps = vec![];
-    for i in 0..bots.len() {
-        let mut overlap = 0;
-        for j in 0..bots.len() {
-            if i == j {
-                continue;
-            }
-            let a = bots[i];
-            let b = bots[j];
-            let mh = manhattan(&a, &b);
-            if a.radius + b.radius >= mh {
-                overlap += 1;
-            }
-        }
-        overlaps.push(overlap);
-    }
-    let max_o = overlaps.iter().max().unwrap();
-    let candidates : HashSet<usize> = HashSet::from_iter(overlaps.iter().enumerate().filter(|(_, c)| *c == max_o).map(|(n, _)| n));
-    let cbots : Vec<Bot> = bots.iter().cloned().enumerate().filter(|(n, _)| candidates.contains(n)).map(|(_, c)| c).collect();
-    cbots
-}
-
-fn fill(a: &Bot, b: &Bot, pos: (i64, i64, i64), intersections: &mut HashSet<(i64, i64, i64)>) {
-    let mut ps = VecDeque::new();
-    ps.push_back(pos);
-    while let Some(pos) = ps.pop_front() {
-        for (dx, dy, dz) in &[(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)] {
-            let c = Bot { x: pos.0 + dx, y: pos.1 + dy, z: pos.2 + dz, radius: 1 };
-            let p = (c.x, c.y, c.z);
-            if intersections.contains(&p) {
-                continue;
-            }
-            let inside_a = manhattan(&c, &a) <= a.radius;
-            let inside_b = manhattan(&c, &b) <= b.radius;
-            if inside_a && inside_b {
-                intersections.insert(p);
-                ps.push_back(p);
-            }
-        }
-        if intersections.len() % 1000 == 0 {
-            println!("intersections: {}", intersections.len());
-        }
-    }
-}
-
-fn overlap(a: &Bot, b: &Bot) -> HashSet<(i64, i64, i64)> {
+fn walk(a: &Bot, b: &Bot) -> Bot {
     // Walk towards the other
     let mut min_d = manhattan(&a, &b);
     let mut p = *a;
@@ -98,6 +45,7 @@ fn overlap(a: &Bot, b: &Bot) -> HashSet<(i64, i64, i64)> {
             let d = manhattan(&c, &b);
             inside_b = d <= b.radius;
             if inside_b {
+                p = c;
                 break;
             }
             if d < min_d {
@@ -107,9 +55,7 @@ fn overlap(a: &Bot, b: &Bot) -> HashSet<(i64, i64, i64)> {
             }
         }
     }
-    let mut intersections = HashSet::new();
-    fill(a, b, (p.x, p.y, p.z), &mut intersections);
-    intersections
+    p
 }
 
 fn solve(path: &Path) {
@@ -118,17 +64,18 @@ fn solve(path: &Path) {
     let bots : Vec<Bot> = buffered.lines().filter_map(Result::ok).map(|a| a.parse::<Bot>().unwrap()).collect();
     let max_r = bots.iter().max_by(|a, b| a.radius.cmp(&b.radius)).unwrap();
     let in_range = bots.iter().filter(|c| manhattan(c, max_r) <= max_r.radius).count();
-    for i in 0..bots.len() {
-        for j in 0..bots.len() {
-            if i == j {
-                continue;
-            }
-            let a = bots[i];
-            let b = bots[j];
-            let intersections = overlap(&a, &b);
-            println!("intersections: {}", intersections.len());
-        }
+    println!("in range: {}", in_range);
+    let origin = Bot{ x: 0, y: 0, z: 0, radius: 1 };
+    let mut candidates = vec![];
+    for a in &bots {
+        let pa = walk(&origin, &a);
+        let inside = bots.iter().filter(|c| manhattan(&pa, &c) <= c.radius).count();
+        let d = manhattan(&pa, &origin);
+        println!("{:?}, {}, {}", pa, inside, d);
+        candidates.push((inside, d, pa));
     }
+    candidates.sort_by(|a, b| b.0.cmp(&a.0).then_with(|| a.1.cmp(&b.1)));
+    println!("{:?}", candidates[0]);
 }
 
 fn main() {

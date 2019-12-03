74bf7b1e02549137bee3e23808417bdb1e9952ba
Pär Bohrarper <par.bohrarper@gmail.com>
2019-12-03 18:21:47 +0100

    refactor day 1 to use one file / binary

--- 2019/01/main.rs
+++ 2019/01/main.rs
@@ -5,6 +5,10 @@ use std::io::prelude::*;
 use std::iter::*;
 use std::path::Path;
 
+fn get_fuel(mass: i64) -> i64 {
+    std::cmp::max((mass / 3) - 2, 0)
+}
+
 fn get_fuel_recursive(mass: i64) -> i64 {
     let fuel = (mass / 3) - 2;
     if fuel <= 0 {
@@ -13,28 +17,46 @@ fn get_fuel_recursive(mass: i64) -> i64 {
     return fuel + get_fuel_recursive(fuel);
 }
 
-fn solve(path: &Path) -> i64 {
+fn part1(masses: &Vec<i64>) -> i64 {
+    masses.iter().map(|&m| get_fuel(m)).sum()
+}
+
+fn part2(masses: &Vec<i64>) -> i64 {
+    masses.iter().map(|&m| get_fuel_recursive(m)).sum()
+}
+
+fn input(path: &Path) -> Vec<i64> {
     let input = File::open(path).unwrap();
     let buffered = BufReader::new(input);
     let lines : Vec<String> = buffered.lines().filter_map(Result::ok).collect();
-    let result = lines.iter().map(|v| v.parse::<i64>().unwrap()).map(|m| get_fuel_recursive(m)).sum();
-    result
+    lines.iter().map(|v| v.parse::<i64>().unwrap()).collect()
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
+    let part = args[1].parse::<i32>().unwrap();
+    let filename = &args[2];
 
-    let result = solve(Path::new(&filename));
+    let parsed = input(Path::new(&filename));
+
+    let result = if part == 1 {
+        part1(&parsed)
+    } else {
+        part2(&parsed)
+    };
     println!("{}", result);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::get_fuel_recursive;
+    use super::{get_fuel, get_fuel_recursive};
 
     #[test]
     fn fuel() {
+        assert_eq!(get_fuel(12), 2);
+        assert_eq!(get_fuel(14), 2);
+        assert_eq!(get_fuel(1969), 654);
+        assert_eq!(get_fuel(100756), 33583);
         assert_eq!(get_fuel_recursive(12), 2);
         assert_eq!(get_fuel_recursive(14), 2);
         assert_eq!(get_fuel_recursive(1969), 966);

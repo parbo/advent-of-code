d5b43489c2070aaad78a140d487de062ca5f489f
Pär Bohrarper <par.bohrarper@gmail.com>
2019-12-03 21:58:50 +0100

    add some stats

--- 2019/intcode/src/lib.rs
+++ 2019/intcode/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 enum Op {
     ADD = 1,
     MUL = 2,
@@ -18,43 +20,50 @@ impl Op {
 
 pub struct Machine {
     memory: Vec<i64>,
-    ip: usize
+    ip: usize,
+    executes: HashMap<usize, usize>,
+    reads: HashMap<usize, usize>,
+    writes: HashMap<usize, usize>,
 }
 
 impl Machine {
     pub fn new(memory: &Vec<i64>) -> Machine {
-        Machine { memory: memory.clone(), ip: 0 }
+        Machine { memory: memory.clone(), ip: 0, executes: HashMap::new(), reads: HashMap::new(), writes: HashMap::new() }
     }
 
     pub fn write(&mut self, pos: usize, value: i64) {
         self.memory[pos] = value;
     }
 
-    fn read_operand(&self, pos: usize) -> Option<&i64> {
-        self.memory.get(*self.memory.get(pos)? as usize)
+    fn read_operand(&mut self, pos: usize) -> Option<&i64> {
+        let addr = *self.memory.get(pos)? as usize;
+        *self.reads.entry(addr).or_insert(0) += 1;
+        self.memory.get(addr)
     }
 
     fn write_operand(&mut self, pos: usize, value: i64) {
         // TODO: error handling
-        let addr = self.memory[pos];
-        self.memory[addr as usize] = value;
+        let addr = self.memory[pos] as usize;
+        *self.writes.entry(addr).or_insert(0) += 1;
+        self.memory[addr] = value;
     }
 
     // Returns instruction size
     pub fn step(&mut self) -> Option<usize> {
         let pos = self.ip;
+        *self.executes.entry(pos).or_insert(0) += 1;
         let op = self.memory.get(pos).and_then(|&x| Op::from_i64(x))?;
         match op {
             Op::ADD => {
-                let v1 = self.read_operand(pos + 1)?;
-                let v2 = self.read_operand(pos + 2)?;
+                let v1 = *self.read_operand(pos + 1)?;
+                let v2 = *self.read_operand(pos + 2)?;
                 let res = v1 + v2;
                 self.write_operand(pos + 3, res);
                 Some(4)
             },
             Op::MUL => {
-                let v1 = self.read_operand(pos + 1)?;
-                let v2 = self.read_operand(pos + 2)?;
+                let v1 = *self.read_operand(pos + 1)?;
+                let v2 = *self.read_operand(pos + 2)?;
                 let res = v1 * v2;
                 self.write_operand(pos + 3, res);
                 Some(4)
@@ -75,6 +84,21 @@ impl Machine {
         }
         Some(*self.memory.get(0)?)
     }
+
+    pub fn dump(&self, n: usize) {
+        let mut exec_vec: Vec<(&usize, &usize)> = self.executes.iter().collect();
+        exec_vec.sort_by(|a, b| b.1.cmp(a.1));
+        let mut read_vec: Vec<(&usize, &usize)> = self.reads.iter().collect();
+        read_vec.sort_by(|a, b| b.1.cmp(a.1));
+        let mut write_vec: Vec<(&usize, &usize)> = self.writes.iter().collect();
+        write_vec.sort_by(|a, b| b.1.cmp(a.1));
+        println!("Executed:");
+        exec_vec.iter().take(n).for_each(|x| println!("  {} - {}", x.0, x.1));
+        println!("Read:");
+        read_vec.iter().take(n).for_each(|x| println!("  {} - {}", x.0, x.1));
+        println!("Written:");
+        write_vec.iter().take(n).for_each(|x| println!("  {} - {}", x.0, x.1));
+    }
 }
 
 #[cfg(test)]
@@ -86,5 +110,6 @@ mod tests {
         let input = vec![1,9,10,3,2,3,11,0,99,30,40,50];
         let mut m = Machine::new(&input);
         assert_eq!(m.run(), Some(3500));
+        m.dump(10);
     }
 }

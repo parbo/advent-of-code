80a4155257bbdbc201b77b482c37088e6dfe7e27
Pär Bohrarper <bohrarper@spotify.com>
2019-12-06 10:25:57 +0100

    refactor

--- 2019/06/main.rs
+++ 2019/06/main.rs
@@ -1,104 +1,36 @@
-use itertools::Itertools;
-use std::collections::HashMap;
-use std::collections::HashSet;
-use std::env;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
+use aoc;
+use aoc::Tree;
 use std::iter::*;
-use std::path::Path;
 
-struct Data {
-    things: HashSet<String>,
-    orbits: HashMap<String, Vec<String>>,
-    thing_orbits: HashMap<String, String>,
+fn part1(tree: &Tree) -> i64 {
+    tree.things()
+        .map(|t| tree.depth_from_to("COM", &t))
+        .sum::<Option<i64>>()
+        .unwrap()
 }
 
-fn tree(obj: &str, end: &str, orbits: &HashMap<String, Vec<String>>, depth: i64) -> i64 {
-    if obj == end {
-        return depth;
-    }
-    if let Some(v) = orbits.get(obj) {
-        for t in v {
-            let x = tree(t, end, orbits, depth + 1);
-            if x != 0 {
-                return x;
-            }
-        }
-    }
-    return 0;
+fn part2(tree: &Tree) -> i64 {
+    tree.things()
+        .filter_map(|t| {
+            [tree.depth_from_to(t, "YOU"), tree.depth_from_to(t, "SAN")].iter().map(|&x| x).sum::<Option<i64>>()
+        })
+        .min()
+        .unwrap()
+        - 2
 }
 
-fn part1(data: &Data) -> i64 {
-    let mut v = 0;
-    for thing in data.things.iter() {
-        let t = tree("COM", &thing, &data.orbits, 0);
-        println!("{}, {:?}", thing, t);
-        v += t;
-    }
-    v
-}
-
-fn part2(data: &Data) -> i64 {
-    let mut dists = HashMap::new();
-    for thing in data.things.iter() {
-        let d1 = tree(thing, "YOU", &data.orbits, 0);
-        let d2 = tree(thing, "SAN", &data.orbits, 0);
-        if d1 > 0 && d2 > 0 {
-            println!("common: {}, {}, {}", thing, d1, d2);
-            dists.insert(thing, (d1, d2));
-        }
-    }
-    let mut s = data.things.len() as i64;
-    let mut st = None;
-    for (thing, d) in dists {
-        let dist = d.0 + d.1;
-        if dist < s {
-            println!("{} -> {}", thing, dist);
-            s = dist;
-            st = Some(thing);
-        }
-    }
-    println!("Min: {:?} -> {}", st, s);
-    s - 2
-}
-
-fn parse_lines(lines: &Vec<String>) -> Data {
-    let mut things: HashSet<String> = HashSet::new();
-    let mut orbits: HashMap<String, Vec<String>> = HashMap::new();
-    let mut thing_orbits: HashMap<String, String> = HashMap::new();
-    for line in lines {
-        let orbit = line
-            .split(')')
-            .map(|x| x.trim().to_string())
-            .collect_tuple::<(String, String)>()
-            .unwrap();
-        things.insert(orbit.0.clone());
-        things.insert(orbit.1.clone());
-        thing_orbits.insert(orbit.1.clone(), orbit.0.clone());
-        orbits.entry(orbit.0).or_insert(Vec::new()).push(orbit.1);
-    }
-    Data {
-        things,
-        orbits,
-        thing_orbits,
-    }
-}
-
-fn input(path: &Path) -> Data {
-    let input = File::open(path).unwrap();
-    let buffered = BufReader::new(input);
-    let lines: Vec<String> = buffered.lines().filter_map(Result::ok).collect();
-    parse_lines(&lines)
+fn parse(lines: &Vec<String>) -> Tree {
+    let mut tree = Tree::new();
+    lines
+        .iter()
+        .map(|line| line.split(')').collect())
+        .for_each(|orbit: Vec<_>| tree.insert(orbit[0], orbit[1]));
+    tree
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let part = args[1].parse::<i32>().unwrap();
-    let filename = &args[2];
-
-    let parsed = input(Path::new(&filename));
-
+    let (part, lines) = aoc::read_lines();
+    let parsed = parse(&lines);
     let result = if part == 1 {
         part1(&parsed)
     } else {
@@ -109,7 +41,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use super::{parse_lines, part1, part2};
+    use super::{parse, part1, part2};
 
     #[test]
     fn test_part1() {
@@ -117,9 +49,9 @@ mod tests {
             "COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L",
         ]
         .iter()
-        .map(|x| x.to_string())
+        .map(|&x| x.into())
         .collect();
-        let d = parse_lines(&lines);
+        let d = parse(&lines);
         assert_eq!(part1(&d), 42);
     }
 
@@ -130,9 +62,9 @@ mod tests {
             "I)SAN",
         ]
         .iter()
-        .map(|x| x.to_string())
+        .map(|&x| x.into())
         .collect();
-        let d = parse_lines(&lines);
+        let d = parse(&lines);
         assert_eq!(part2(&d), 4);
     }
 }

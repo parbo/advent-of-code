180b2134dd7df77b586a7641ee044abcbf5654aa
Pär Bohrarper <bohrarper@spotify.com>
2019-12-09 10:21:45 +0100

    fix some template + aoc intcode parsing

--- aoc/src/lib.rs
+++ aoc/src/lib.rs
@@ -69,3 +69,12 @@ pub fn read_lines() -> (i32, Vec<String>) {
             .collect(),
     )
 }
+
+pub fn parse_intcode(lines: &Vec<String>) -> Vec<i128> {
+    let result: Vec<i128> = lines[0]
+        .split(|c| c == ',')
+        .map(|s| s.trim())
+        .map(|v| v.parse::<i128>().unwrap())
+        .collect();
+    result
+}

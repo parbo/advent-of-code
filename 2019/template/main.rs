5c077e0270e189cd3d6e1a27b7eab9b7028334bf
Pär Bohrarper <par.bohrarper@gmail.com>
2019-12-11 18:50:09 +0100

    streamline template

--- 2019/template/main.rs
+++ 2019/template/main.rs
@@ -1,23 +1,22 @@
 use aoc;
+// use intcode;
 use std::iter::*;
 
-fn part1(things: &Vec<i64>) -> i64 {
-    things[0]
+fn part1(_: &Vec<i64>) -> i64 {
+    0
 }
 
-fn part2(things: &Vec<i64>) -> i64 {
-    things[0]
+fn part2(_: &Vec<i64>) -> i64 {
+    0
 }
 
 fn parse(lines: &Vec<String>) -> Vec<i64> {
-    lines
-        .iter()
-        .map(|x| x.parse::<i64>().unwrap())
-        .collect()
+    lines.iter().map(|x| x.parse::<i64>().unwrap()).collect()
 }
 
 fn main() {
     let (part, lines) = aoc::read_lines();
+    //let parsed = aoc::parse_intcode(&lines);
     let parsed = parse(&lines);
     let result = if part == 1 {
         part1(&parsed)
@@ -29,15 +28,10 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use super::{part1, part2};
+    // use super::part1;
 
-    #[test]
-    fn test_part1() {
-        assert_eq!(part1(&vec![0]), 0);
-    }
-
-    #[test]
-    fn test_part2() {
-        assert_eq!(part2(&vec![0]), 0);
-    }
+    // #[test]
+    // fn test_part1() {
+    //     assert_eq!(part1(&vec![0]), 0);
+    // }
 }

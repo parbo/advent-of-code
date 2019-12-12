e779936fafaa2b6cb653de2d3cc7513f87d075d4
Pär Bohrarper <par.bohrarper@gmail.com>
2019-12-12 18:12:38 +0100

    add disassembly of day 11

--- 2019/11/main.rs
+++ 2019/11/main.rs
@@ -75,15 +75,7 @@ fn part1(numbers: &Vec<i128>) -> i128 {
     hull.iter().count() as i128
 }
 
-fn part2(numbers: &Vec<i128>) -> i128 {
-    let window = initscr();
-    nl();
-    noecho();
-    curs_set(0);
-    window.keypad(true);
-    window.scrollok(true);
-    window.timeout(20);
-    let hull = paint(numbers, 1, Some(&window));
+fn print_hull(hull: &HashMap<(i128, i128), i128>) {
     let min_x = hull.iter().map(|p| (p.0).0).min().unwrap();
     let min_y = hull.iter().map(|p| (p.0).1).min().unwrap();
     let max_x = hull.iter().map(|p| (p.0).0).max().unwrap();
@@ -102,7 +94,147 @@ fn part2(numbers: &Vec<i128>) -> i128 {
         }
         println!();
     }
+}
+
+fn part2(numbers: &Vec<i128>) -> i128 {
+    let window = initscr();
+    nl();
+    noecho();
+    curs_set(0);
+    window.keypad(true);
+    window.scrollok(true);
+    window.timeout(20);
+    let hull = paint(numbers, 1, Some(&window));
+    print_hull(&hull);
     0
+}
+
+/////// Disassembled input
+
+struct Disassembled {
+    outs: Vec<i128>,
+    v485: usize,
+    v486: [i128;4],
+}
+
+impl Disassembled {
+    pub fn new() -> Disassembled {
+        Disassembled { outs: Vec::new(), v485: 0, v486: [1, 0, 0, 1] }
+    }
+
+    fn out(&mut self, a: i128) {
+        self.outs.push(a);
+    }
+
+    fn input(&mut self) {
+    }
+
+    pub fn run(&mut self) {
+        self.out(0);
+        self.out(1);
+        self.f459(48092525312);
+        self.f459(665750184716);
+        self.input();
+        self.out(0);
+        self.out(1);
+        self.input();
+        self.out(0);
+        self.out(0);
+        self.input();
+        self.out(0);
+        self.out(1);
+        self.input();
+        self.out(0);
+        self.out(1);
+        self.input();
+        self.out(0);
+        self.out(0);
+        self.input();
+        self.out(0);
+        self.out(1);
+        self.f459(235324768296);
+        self.f459(3263212736);
+        self.input();
+        self.out(0);
+        self.out(0);
+        self.input();
+        self.out(0);
+        self.out(0);
+        self.f459(709496824676);
+        self.f459(988220904204);
+
+        let mut current_dir = Dir::Up;
+        let mut x = 0;
+        let mut y = 0;
+        let mut hull = HashMap::new();
+        let mut i = self.outs.iter();
+        loop {
+            let maybe_color = i.next();
+            let maybe_turn = i.next();
+            if maybe_color.is_none() || maybe_turn.is_none() {
+                break;
+            }
+            let color = *maybe_color.unwrap();
+            let turn = *maybe_turn.unwrap();
+            hull.insert((x, y), color);
+            current_dir = match current_dir {
+                Dir::Up => if turn == 0 { Dir::Left } else { Dir::Right },
+                Dir::Right => if turn == 0 { Dir::Up } else { Dir::Down },
+                Dir::Down => if turn == 0 { Dir::Right } else { Dir::Left },
+                Dir::Left => if turn == 0 { Dir::Down } else { Dir::Up },
+            };
+            match current_dir {
+                Dir::Up => y -= 1,
+                Dir::Right => x += 1,
+                Dir::Down => y += 1,
+                Dir::Left => x -= 1,
+            }
+        }
+        print_hull(&hull);
+    }
+
+    fn f459(&mut self, a1: i128) {
+        self.f523(490, 40, a1);
+    }
+
+
+    fn f490(&mut self, a1: i128) {
+        let _ = self.input(); // input unused
+        self.out(a1);
+        self.out(self.v486[self.v485]);
+        self.v485 = self.v485 + 1;
+        if 4 == self.v485 {
+            self.v485 = 0
+        }
+    }
+
+    fn f523(&mut self, _a1: i128, a2: i128, mut a3: i128) {
+        //v522 = a1
+        if a3 < 0 {
+            a3 = 0
+        }
+        self.f564(0, 1, a2, a3);
+    }
+
+    fn f564(&mut self, mut a1: i128, mut a2: i128, a3: i128, mut a4: i128) -> i128 {
+        if a3 < 1 {
+            if a4 < a2 {
+                return a4;
+            }
+        }
+        a4 = self.f564(a1, 2 * a2, a3 - 1, a4);
+        a1 = 1;
+        if a4 < a2 {
+            a1 = 0;
+            a2 = 0;
+        }
+        if 0 < a3 {
+            self.f490(a1);  // v522
+        }
+        a2 = a2 * -1;
+        a4 = a4 + a2;
+        return a4
+    }
 }
 
 fn main() {
@@ -114,5 +246,8 @@ fn main() {
         part2(&parsed)
     };
     println!("{}", result);
+
+    let mut d = Disassembled::new();
+    d.run();
 }
 

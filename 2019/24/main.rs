64c139a3f95ef857045e00083c49e591945bb152
Pär Bohrarper <par.bohrarper@gmail.com>
2019-12-24 11:34:18 +0100

    wip

--- 2019/24/main.rs
+++ 2019/24/main.rs
@@ -2,6 +2,7 @@ use aoc;
 // use intcode;
 use std::iter::*;
 use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn bd(grid: &Vec<Vec<char>>) -> i64 {
     let h = grid.len();
@@ -23,7 +24,63 @@ fn bd(grid: &Vec<Vec<char>>) -> i64 {
 }
 
 fn part2(grid: &Vec<Vec<char>>) -> i64 {
-    loop {}
+    let mut g = HashMap::new();
+    let mut yy = -2;
+    for y in 0..5 {
+        let mut xx = -2;
+        for x in 0..5 {
+            g.insert((0, xx, yy), grid[y][x]);
+            xx += 1;
+        }
+        yy += 1;
+    }
+    let mut mins = 0;
+    loop {
+        let mut new_g = g.clone();
+        let mut any_bug = true;
+
+        let mut yy :i64 = 0;
+        let mut all_bugs = 0;
+        while any_bug {
+            let mut xx:i64 = 0;
+            while any_bug {
+
+                let mut tot_c = 0;
+                for (x, y) in &[(xx, yy), (xx,-yy), (-xx,yy),(-xx,-yy)] {
+                    let level : i64 = std::cmp::max((x / 3).abs(), (y /3).abs());
+                    let mut c = 0;
+  		    for (nx, ny) in &[(*x + 1, *y), (*x - 1, *y), (*x, *y + 1), (*x, *y - 1)] {
+                        let nblevel : i64 = std::cmp::max((nx / 3).abs(), (ny /3).abs());
+                        if let Some(x) = g.get(&(nblevel, nx % 3, ny % 3)) {
+                            if *x == '#' {
+                                c += 1;
+                            }
+                        }
+                    }
+                    let a = new_g.entry((level, x % 3, y % 3)).or_insert('.');
+                    if *a == '.' {
+                        if c == 1 || c == 2 {
+                            *a = '#';
+                        }
+                    } else {
+                        if c == 1 {
+                            *a = '#';
+                        }
+                    }
+                    tot_c += c;
+                }
+                any_bug = tot_c > 0;
+                all_bugs += tot_c;
+                xx += 1;
+              
+            }
+            yy += 1;
+        }
+        mins += 1;
+        if mins == 200 {
+            return all_bugs;
+        }
+    }
 }
 
 fn part1(grid: &Vec<Vec<char>>) -> i64 {

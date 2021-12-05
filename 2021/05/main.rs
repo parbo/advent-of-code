b483ea0fadb722c9cd8161639e9fd94e5bd4dacc
Pär Bohrarper <par.bohrarper@gmail.com>
2021-12-05 10:57:12 +0100

    add day5 visualization

--- 2021/05/main.rs
+++ 2021/05/main.rs
@@ -54,13 +54,52 @@ fn parse(lines: &[String]) -> Parsed {
         .collect()
 }
 
+fn draw(lines: &[Line]) -> Answer {
+    let mut counts = HashMap::new();
+    lines
+        .iter()
+        .map(|line| aoc::plot_line(line.a, line.b))
+        .for_each(|points| {
+            points
+                .into_iter()
+                .for_each(|p| *counts.entry(p).or_insert(0) += 1)
+        });
+    let min_x = counts.keys().map(|x| x[0]).min().unwrap();
+    let max_x = counts.keys().map(|x| x[0]).max().unwrap();
+    let min_y = counts.keys().map(|x| x[1]).min().unwrap();
+    let max_y = counts.keys().map(|x| x[1]).max().unwrap();
+    let mut gd = aoc::BitmapGridDrawer::new(
+        |x| match x {
+            0 => [0, 0, 0],
+            1 => [0, 255, 0],
+            _ => [255, 0, 0],
+        },
+        "ppm/day05",
+    );
+    gd.set_rect(([min_x, min_y], [max_x, max_y]));
+    counts.clear();
+    lines
+        .iter()
+        .map(|line| aoc::plot_line(line.a, line.b))
+        .for_each(|points| {
+            points.into_iter().for_each(|p| {
+                *counts.entry(p).or_insert(0) += 1;
+            });
+            gd.draw_grid(&counts);
+            gd.save_image();
+        });
+    0
+}
+
 fn main() {
     let (part, lines) = aoc::read_lines();
     let parsed = parse(&lines);
     let result = if part == 1 {
         part1(&parsed)
-    } else {
+    } else if part == 2 {
         part2(&parsed)
+    } else {
+        draw(&parsed)
     };
     println!("{}", result);
 }

f3e5576d0689df33e1c7decdf7e8b3c1bf2a4e2d
Pär Bohrarper <par.bohrarper@gmail.com>
2021-12-06 07:59:11 +0100

    clean up and format

--- 2021/06/main.rs
+++ 2021/06/main.rs
@@ -1,42 +1,35 @@
-use std::{iter::*, collections::VecDeque};
+use std::{collections::VecDeque, iter::*};
 
 type ParsedItem = i64;
 type Parsed = Vec<ParsedItem>;
 type Answer = i64;
 
-fn part1(f: &[ParsedItem]) -> Answer {
-    let mut fish = f.to_owned();
-    for _i in 0..80 {
-	let mut n = vec![];
-	for f in &mut fish {
-	    match f {
-		0 => {
-		    *f = 6;
-		    n.push(8);
-		},
-		_ => *f -= 1,
-	    }
-	}
-	fish.append(&mut n);
-    }
-    fish.len() as Answer
-}
-
-fn part2(f: &[ParsedItem]) -> Answer {
-    let mut fish: VecDeque<i64> = VecDeque::from([0,0,0,0,0,0,0,0,0]);
+fn solve(f: &[ParsedItem], days: i32) -> Answer {
+    let mut fish: VecDeque<i64> = VecDeque::from([0, 0, 0, 0, 0, 0, 0, 0, 0]);
     for ff in f {
-	fish[*ff as usize] += 1;
+        fish[*ff as usize] += 1;
     }
-    for i in 0..256 {
-	let born = fish.pop_front().unwrap();
-	fish[6] += born;
-	fish.push_back(born);
+    for _i in 0..days {
+        let born = *fish.front().unwrap();
+        fish.rotate_left(1);
+        fish[6] += born;
     }
     fish.iter().sum::<i64>() as Answer
 }
 
+fn part1(f: &[ParsedItem]) -> Answer {
+    solve(f, 80)
+}
+
+fn part2(f: &[ParsedItem]) -> Answer {
+    solve(f, 256)
+}
+
 fn parse(lines: &[String]) -> Parsed {
-    aoc::split_ch(&lines[0], ',').iter().map(|x| x.parse().unwrap()).collect()
+    aoc::split_ch(&lines[0], ',')
+        .iter()
+        .map(|x| x.parse().unwrap())
+        .collect()
 }
 
 fn main() {
@@ -56,11 +49,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(&vec![3,4,3,1,2]), 5934);
+        assert_eq!(part1(&vec![3, 4, 3, 1, 2]), 5934);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(&vec![3,4,3,1,2]), 26984457539);
+        assert_eq!(part2(&vec![3, 4, 3, 1, 2]), 26984457539);
     }
 }

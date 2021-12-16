2f63278df5df563f85286e7375c3a5254041278b
Pär Bohrarper <par.bohrarper@gmail.com>
2021-12-16 09:41:29 +0100

    cleanup

--- 2021/16/main.rs
+++ 2021/16/main.rs
@@ -7,204 +7,131 @@ type Answer = i64;
 struct Packet {
     version: u8,
     id: u8,
-    data: (u32, u32), // (offs, len)
     value: Option<i64>,
-    children: Vec<Box<Packet>>,
+    packets: Vec<Packet>,
 }
 
-fn parse_packet(bytes: &Vec<u8>, bp: u32, c: usize) -> Packet {
-    // println!("{:indent$}bp: {}", "", bp, indent=c*2);
+fn parse_packet(bytes: &Vec<u8>, bp: u32, c: usize) -> (Packet, u32) {
     let version = bytes.get_u8(0, bp, 3).unwrap();
     let id = bytes.get_u8(0, bp + 3, 3).unwrap();
-    // println!("{:indent$}parse: {}, {}, {}", "", bp, version, id, indent=c*2);
-    match id {
-        4u8 => {
-            let mut pos = bp + 6;
-            let mut num: i64 = 0;
-            loop {
-                let cont = bytes.get_u8(0, pos, 1).unwrap();
-                let nybble = bytes.get_u8(0, pos + 1, 4).unwrap() as i64;
-                pos += 5;
-                num <<= 4;
-                num += nybble;
-                if cont == 0u8 {
-                    break;
-                }
+    if let 4u8 = id {
+        let mut pos = bp + 6;
+        let mut num: i64 = 0;
+        loop {
+            let cont = bytes.get_u8(0, pos, 1).unwrap();
+            let nybble = bytes.get_u8(0, pos + 1, 4).unwrap() as i64;
+            pos += 5;
+            num <<= 4;
+            num += nybble;
+            if cont == 0u8 {
+                break;
             }
-            let plen = pos - (bp + 6);
-            //println!("{:indent$}literal: {}, {}, {}, {}", "", num, bp + 6, pos, plen, indent=c*2);
+        }
+        (
             Packet {
                 version,
                 id,
-                data: (bp + 6, plen),
                 value: Some(num),
-                children: vec![],
-            }
-        }
-        _ => {
-            let length_type = bytes.get_u8(0, bp + 6, 1).unwrap();
-            //println!("{:indent$}lt: {}", "", length_type, indent=c*2);
-            match length_type {
-                0 => {
-                    let mut bits = bytes.get_u16(0, bp + 7, 15).unwrap() as u32;
-                    let mut pos = bp + 22;
-                    let mut packets = vec![];
-                    while bits > 0 {
-                        //println!("{:indent$}parse child.. bits left: {}", "", bits, indent=c*2);
-                        let p = parse_packet(bytes, pos, c + 1);
-                        pos += 6 + p.data.1;
-                        bits -= 6 + p.data.1;
-                        packets.push(Box::new(p));
-			// Smallest packet is 11 bits
-			if pos as usize + 11 >= bytes.len() * 8 {
-			    //println!("{:indent$}no more data!", "", indent=c*2);
-			    break;
-			}
-                    }
-                    let plen = pos - (bp + 6);
-                    //println!("{:indent$}children: {}, plen: {}", "", packets.len(), plen, indent=c*2);
-                    Packet {
-                        version,
-                        id,
-                        data: (bp + 6, plen),
-                        value: None,
-                        children: packets,
-                    }
-                }
-                _ => {
-                    let num = bytes.get_u16(0, bp + 7, 11).unwrap();
-                    //println!("{:indent$}num: {}", "", num, indent=c*2);
-                    let mut packets = vec![];
-                    let mut pos = bp + 18;
-                    for _i in 0..num {
-                        //println!("{:indent$}parse child {}..", "", i, indent=c*2);
-                        let p = parse_packet(bytes, pos, c + 1);
-                        pos += 6 + p.data.1;
-                        //println!("{:indent$}pos: {}", "", pos, indent=c*2);
-                        packets.push(Box::new(p));
-                    }
-                    let plen = pos - (bp + 6);
-                    //println!("{:indent$}children: {}", "", packets.len(), indent=c*2);
-                    Packet {
-                        version,
-                        id,
-                        data: (bp + 6, plen),
-                        value: None,
-                        children: packets,
-                    }
+                packets: vec![],
+            },
+            pos - (bp + 6),
+        )
+    } else {
+        let length_type = bytes.get_u8(0, bp + 6, 1).unwrap();
+        if let 0 = length_type {
+            let mut bits = bytes.get_u16(0, bp + 7, 15).unwrap() as u32;
+            let mut pos = bp + 22;
+            let mut packets = vec![];
+            while bits > 0 {
+                let (p, len) = parse_packet(bytes, pos, c + 1);
+                pos += 6 + len;
+                bits -= 6 + len;
+                packets.push(p);
+                if pos as usize + 11 >= bytes.len() * 8 {
+                    break;
                 }
             }
+            (
+                Packet {
+                    version,
+                    id,
+                    value: None,
+                    packets,
+                },
+                pos - (bp + 6),
+            )
+        } else {
+            let num = bytes.get_u16(0, bp + 7, 11).unwrap();
+            let mut packets = vec![];
+            let mut pos = bp + 18;
+            for _i in 0..num {
+                let (p, len) = parse_packet(bytes, pos, c + 1);
+                pos += 6 + len;
+                packets.push(p);
+            }
+            (
+                Packet {
+                    version,
+                    id,
+                    value: None,
+                    packets,
+                },
+                pos - (bp + 6),
+            )
         }
     }
 }
 
 fn sum_version(packet: &Packet) -> i64 {
     let mut v = packet.version as i64;
-    for p in &packet.children {
-        v += sum_version(&p)
+    for p in &packet.packets {
+        v += sum_version(p)
     }
     v
 }
 
 fn calculate(packet: &Packet) -> i64 {
     match packet.id {
-	0 => {
-	    // sum
-	    print!("sum(");
-	    let v = packet.children.iter().map(|c| {
-		let c = calculate(c);
-		print!(",");
-		c
-	    }).sum();
-	    print!(")");
-	    v
-	}
-	1 => {
-	    // product
-	    print!("product(");
-	    let v = packet.children.iter().map(|c| {
-		let c = calculate(c);
-		print!(",");
-		c
-	    }).product();
-	    print!(")");
-	    v
-	}
-	2 => {
-	    // min
-	    print!("min(");
-	    let v = packet.children.iter().map(|c| {
-		let c = calculate(c);
-		print!(",");
-		c
-	    }).min().unwrap();
-	    print!(")");
-	    v
-	}
-	3 => {
-	    // max
-	    print!("max(");
-	    let v = packet.children.iter().map(|c| {
-		let c = calculate(c);
-		print!(",");
-		c
-	    }).max().unwrap();
-	    print!(")");
-	    v
-	}
-	4 => {
-	    // literal
-	    print!("{}", packet.value.unwrap() as i64);
-	    packet.value.unwrap() as i64
-	}
-	5 => {
-	    // gt
-	    let left = calculate(&packet.children[0]);
-	    print!(" > ");
-	    let right = calculate(&packet.children[1]);
-	    assert_eq!(packet.children.len(), 2);
-	    if left > right {
-		1
-	    } else {
-		0
-	    }
-	}
-	6 => {
-	    // lt
-	    let left = calculate(&packet.children[0]);
-	    print!(" < ");
-	    let right = calculate(&packet.children[1]);
-	    assert_eq!(packet.children.len(), 2);
-	    if left < right {
-		1
-	    } else {
-		0
-	    }
-	}
-	7 => {
-	    // eq
-	    let left = calculate(&packet.children[0]);
-	    print!(" == ");
-	    let right = calculate(&packet.children[1]);
-	    assert_eq!(packet.children.len(), 2);
-	    if left == right {
-		1
-	    } else {
-		0
-	    }
-	}
-	_ => panic!()
+        0 => packet.packets.iter().map(|c| calculate(c)).sum(),
+        1 => packet.packets.iter().map(|c| calculate(c)).product(),
+        2 => packet.packets.iter().map(|c| calculate(c)).min().unwrap(),
+        3 => packet.packets.iter().map(|c| calculate(c)).max().unwrap(),
+        4 => packet.value.unwrap(),
+        5 => {
+            assert_eq!(packet.packets.len(), 2);
+            if calculate(&packet.packets[0]) > calculate(&packet.packets[1]) {
+                1
+            } else {
+                0
+            }
+        }
+        6 => {
+            assert_eq!(packet.packets.len(), 2);
+            if calculate(&packet.packets[0]) < calculate(&packet.packets[1]) {
+                1
+            } else {
+                0
+            }
+        }
+        7 => {
+            assert_eq!(packet.packets.len(), 2);
+            if calculate(&packet.packets[0]) == calculate(&packet.packets[1]) {
+                1
+            } else {
+                0
+            }
+        }
+        _ => panic!(),
     }
 }
 
-
 fn part1(bytes: &Vec<u8>) -> Answer {
-    let packet = parse_packet(bytes, 0, 0);
+    let (packet, _len) = parse_packet(bytes, 0, 0);
     sum_version(&packet)
 }
 
 fn part2(bytes: &Vec<u8>) -> Answer {
-    let packet = parse_packet(bytes, 0, 0);
+    let (packet, _len) = parse_packet(bytes, 0, 0);
     calculate(&packet)
 }
 
@@ -246,36 +173,36 @@ mod tests {
     #[test]
     fn test_parse_packet_1() {
         let data = parse(&vec!["D2FE28".into()]);
-        let p = parse_packet(&data, 0, 0);
+        let (p, len) = parse_packet(&data, 0, 0);
+        assert_eq!(len, 15);
         assert_eq!(p.id, 4u8);
         assert_eq!(p.version, 6u8);
-        assert_eq!(p.data, (6, 15));
         assert_eq!(p.value, Some(2021));
     }
 
     #[test]
     fn test_parse_packet_2() {
         let data = parse(&vec!["38006F45291200".into()]);
-        let p = parse_packet(&data, 0, 0);
+        let (p, len) = parse_packet(&data, 0, 0);
+        assert_eq!(len, 43);
         assert_eq!(p.id, 6u8);
         assert_eq!(p.version, 1u8);
-        assert_eq!(p.children.len(), 2);
-	assert_eq!((*p.children[0]).value, Some(10));
-	assert_eq!((*p.children[1]).value, Some(20));
-        assert_eq!(p.data, (6, 43));
+        assert_eq!(p.packets.len(), 2);
+        assert_eq!(p.packets[0].value, Some(10));
+        assert_eq!(p.packets[1].value, Some(20));
     }
 
     #[test]
     fn test_parse_packet_3() {
         let data = parse(&vec!["EE00D40C823060".into()]);
-        let p = parse_packet(&data, 0, 0);
+        let (p, len) = parse_packet(&data, 0, 0);
+        assert_eq!(len, 45);
         assert_eq!(p.id, 3u8);
         assert_eq!(p.version, 7u8);
-        assert_eq!(p.children.len(), 3);
-	assert_eq!((*p.children[0]).value, Some(1));
-	assert_eq!((*p.children[1]).value, Some(2));
-	assert_eq!((*p.children[2]).value, Some(3));
-        assert_eq!(p.data, (6, 45));
+        assert_eq!(p.packets.len(), 3);
+        assert_eq!(p.packets[0].value, Some(1));
+        assert_eq!(p.packets[1].value, Some(2));
+        assert_eq!(p.packets[2].value, Some(3));
     }
 
     #[test]
@@ -301,7 +228,10 @@ mod tests {
 
     #[test]
     fn test_part1_4() {
-        assert_eq!(part1(&parse(&vec!["A0016C880162017C3686B18A3D4780".into()])), 31);
+        assert_eq!(
+            part1(&parse(&vec!["A0016C880162017C3686B18A3D4780".into()])),
+            31
+        );
     }
 
     #[test]

12451532daa65ee749b4b1695255a065590f89cd
Pär Bohrarper <par.bohrarper@gmail.com>
2021-12-20 09:57:30 +0100

    day20 done

--- 2021/20/main.rs
+++ 2021/20/main.rs
@@ -1,4 +1,4 @@
-use aoc::*;
+use aoc::{BitmapSpriteGridDrawer, Grid, GridDrawer, Point};
 use std::collections::HashMap;
 use std::iter::*;
 use std::time::Instant;
@@ -8,57 +8,110 @@ type Answer = i64;
 
 pub fn kernel(p: Point) -> impl Iterator<Item = Point> {
     let mut diter = [
-        NORTH_WEST,
-        NORTH,
-        NORTH_EAST,
-        WEST,
+        aoc::NORTH_WEST,
+        aoc::NORTH,
+        aoc::NORTH_EAST,
+        aoc::WEST,
         [0, 0],
-        EAST,
-        SOUTH_WEST,
-        SOUTH,
-        SOUTH_EAST,
+        aoc::EAST,
+        aoc::SOUTH_WEST,
+        aoc::SOUTH,
+        aoc::SOUTH_EAST,
     ]
     .iter();
-    from_fn(move || diter.next().map(|d| point_add(p, *d)))
+    from_fn(move || diter.next().map(|d| aoc::point_add(p, *d)))
 }
 
-fn part1(map: &Parsed) -> Answer {
+fn solve(map: &Parsed, iterations: usize, draw: bool) -> Answer {
+    let mut inf_c = '.';
     let mut g = map.1.clone();
-    let mut gd = aoc::PrintGridDrawer::new(|x| x);
-    gd.draw(&g);
-    println!();
-    for _i in 0..2 {
-        let mut new_g = g.clone();
-        let ([min_x, min_y], [max_x, max_y]) = g.extents();
-        for x in (min_x - 3)..=(max_x + 3) {
-            for y in (min_y - 3)..=(max_y + 3) {
+    let mut all_g = vec![(g.clone(), inf_c)];
+    let mut max_ext = g.extents();
+    for _i in 0..iterations {
+        let mut new_g = HashMap::new();
+        let ext = g.extents();
+        let ([min_x, min_y], [max_x, max_y]) = ext;
+        max_ext.0[0] = min_x.min(max_ext.0[0]);
+        max_ext.0[1] = min_y.min(max_ext.0[1]);
+        max_ext.1[0] = max_x.max(max_ext.1[0]);
+        max_ext.1[1] = max_y.max(max_ext.1[1]);
+        // First do the points inside the extent
+        for x in (min_x - 1)..=(max_x + 1) {
+            for y in (min_y - 1)..=(max_y + 1) {
                 let p = [x, y];
                 let mut ix = 0;
                 for pp in kernel(p) {
-                    let v = if let Some(x) = g.get_value(pp) {
-                        if x == '#' {
-                            1
+                    let c = if aoc::inside_extent(pp, ext) {
+                        if let Some(x) = g.get_value(pp) {
+                            x
                         } else {
-                            0
+                            '.'
                         }
                     } else {
-                        0
+                        inf_c
                     };
+                    let v = (c == '#') as usize;
                     ix <<= 1;
                     ix += v;
                 }
-                new_g.set_value(p, map.0[ix]);
+                if map.0[ix] == '#' {
+                    new_g.set_value(p, '#');
+                }
             }
         }
+        // Update inf_c
+        if inf_c == '#' {
+            inf_c = map.0[511];
+        } else {
+            inf_c = map.0[0];
+        }
         g = new_g;
-        gd.draw(&g);
-        println!();
+        if draw {
+            all_g.push((g.clone(), inf_c));
+        }
+    }
+    if draw {
+        let mut gd = BitmapSpriteGridDrawer::new(
+            (1, 1),
+            |c| {
+                if c == '#' {
+                    vec![[0xff, 0xff, 0xff]; 1]
+                } else {
+                    vec![[0, 0, 0]; 1]
+                }
+            },
+            "ppm/day20",
+        );
+        gd.set_rect(max_ext);
+        gd.set_unset('.');
+        for (gg, ic) in all_g {
+            let mut ggg = HashMap::new();
+            let ext = gg.extents();
+            for x in max_ext.0[0]..=max_ext.1[0] {
+                for y in max_ext.0[1]..=max_ext.1[1] {
+                    let p = [x, y];
+                    if aoc::inside_extent(p, ext) {
+                        if let Some(v) = gg.get_value(p) {
+                            ggg.set_value(p, v)
+                        }
+                    } else {
+                        ggg.set_value(p, ic)
+                    }
+                }
+            }
+            gd.draw(&ggg);
+            gd.save_image();
+        }
     }
     g.iter().filter(|(_p, x)| **x == '#').count() as Answer
 }
 
-fn part2(_: &Parsed) -> Answer {
-    0
+fn part1(map: &Parsed) -> Answer {
+    solve(map, 2, false)
+}
+
+fn part2(map: &Parsed) -> Answer {
+    solve(map, 50, false)
 }
 
 fn parse(lines: &[String]) -> Parsed {
@@ -76,8 +129,12 @@ fn main() {
     let parse_time = Instant::now();
     let result = if part == 1 {
         part1(&parsed)
-    } else {
+    } else if part == 2 {
         part2(&parsed)
+    } else if part == 3 {
+        solve(&parsed, 50, true)
+    } else {
+        solve(&parsed, part as usize, true)
     };
     let done_time = Instant::now();
     println!(

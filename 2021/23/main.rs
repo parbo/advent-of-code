62f81b784cb56dc2cb9faea2daa7954d75199195
Pär Bohrarper <par.bohrarper@gmail.com>
2021-12-23 10:50:49 +0100

    wip

--- 2021/23/main.rs
+++ 2021/23/main.rs
@@ -1,98 +1,100 @@
 use aoc::{Grid, GridDrawer};
-use std::collections::{BinaryHeap, HashMap};
-use std::time::Instant;
 use std::cmp::Reverse;
+use std::collections::{BinaryHeap, BTreeMap, HashSet};
+use std::time::Instant;
 
 type ParsedItem = Vec<char>;
 type Parsed = Vec<ParsedItem>;
 type Answer = i64;
 
 fn get_path(grid: &Parsed, s: aoc::Point, g: aoc::Point) -> Option<(i64, Vec<aoc::Point>)> {
-    aoc::dijkstra_grid(
-	grid,
-	|_p, c| *c == '.',
-	|_pa, _va, _pb, _pv| Some(1),
-	s,
-	g)
+    aoc::dijkstra_grid(grid, |_p, c| *c == '.', |_pa, _va, _pb, _pv| Some(1), s, g)
 }
 
 fn solve(
     grid: &Parsed,
-    goals: &HashMap<char, Vec<aoc::Point>>,
-    init_pos: &[(char, aoc::Point)],
+    goals: &BTreeMap<char, Vec<aoc::Point>>,
+    init_pos: &BTreeMap<aoc::Point, char>,
 ) -> Option<i64> {
     let mut todo = BinaryHeap::new();
-    todo.push(Reverse((0, init_pos.to_owned())));
-//    let mut visited: HashSet<Vec<(char, aoc::Point)>> = HashSet::new();
+    todo.push(Reverse((0, init_pos.clone())));
+    let mut visited = HashSet::new();
     while let Some(Reverse((energy, pos))) = todo.pop() {
-	println!("{}, {:?}", energy, pos);
-        // if visited.contains(&pos) {
-        //     continue;
-        // }
-	// Are all in goals?
-	let mut ok = true;
-	for (a, p) in &pos {
-	    if goals.get(a).unwrap().contains(p) {
-		ok = false;
-		break;
-	    }
-	}
-	if ok {
-	    return Some(energy);
-	}
-	// Nope, find moves
-	for (a, p) in &pos {
-	    let mut moves: Vec<aoc::Point> = vec![];
-	    if goals.get(a).unwrap().contains(p) {
-		// Already in the right spot
-		continue
-	    }
-	    // Are we in the hallway?
-	    if p[0] > 0 && p[0] < 12 && p[1] == 1 {
-		// Find paths to goal
-		if let Some(agoals) = goals.get(a) {
-		    for g in agoals {
-			if let Some((_, _)) = get_path(grid, *p, *g) {
-			    // goal reached, move there
-			    // println!("move to goal");
-			    moves.push(*g);
+        println!("{}, {:?}", energy, pos);
+        if visited.contains(&pos) {
+            continue;
+        }
+	visited.insert(pos.clone());
+        // Are all in goals?
+        let mut ok = true;
+        for (p, a) in &pos {
+            if goals.get(a).unwrap().contains(p) {
+                ok = false;
+                break;
+            }
+        }
+        if ok {
+            return Some(energy);
+        }
+        // Nope, find moves
+        for (p, a) in &pos {
+            let mut moves: Vec<aoc::Point> = vec![];
+            if goals.get(a).unwrap().contains(p) {
+                // Already in the right spot
+                continue;
+            }
+            // Are we in the hallway?
+            if p[0] > 0 && p[0] < 12 && p[1] == 1 {
+                // Find paths to goal
+                if let Some(agoals) = goals.get(a) {
+                    for g in agoals {
+                        let ok = if g[1] == 3 {
+			    // goal empty and same underneath
+                            *pos.get(g).unwrap_or(&'.') == '.'
+                                && *pos.get(&[g[0], g[1] + 1]).unwrap_or(&'.') == *a
+                        } else {
+			    // goal empty
+                            *pos.get(g).unwrap_or(&'.') == '.'
+                        };
+			if ok {
+                            if let Some((_, _)) = get_path(grid, *p, *g) {
+				// goal reached, move there
+				// println!("move to goal");
+				moves.push(*g);
+                            }
 			}
-		    }
-		}
-	    } else {
-		// Try all possible moves out
-		for x in 1..=11 {
-		    let hp = [x, 1];
+                    }
+                }
+            } else {
+                // Try all possible moves out
+                for x in [1, 2, 4, 6, 8, 10, 11] {
+                    let hp = [x, 1];
                     if let Some((_, _)) = get_path(grid, *p, hp) {
-			// hallway reached, move there
-			// println!("move to hallway");
-			moves.push(hp);
-		    }
-		}
-	    }
-	    // if moves.is_empty() {
-	    // 	println!("NO MOVES!");
-	    // }
-	    for mv in moves {
-		let mut new_pos = pos.clone();
-		for (aa, pp) in new_pos.iter_mut() {
-		    if *aa == *a && *pp == *p {
-			*pp = mv;
-			break;
-		    }
-		}
-		// println!("old-pos: {:?}", pos);
-		// println!("new-pos: {:?}", new_pos);
-		let e = match a {
-		    'A' => 1,
-		    'B' => 10,
-		    'C' => 100,
-		    'D' => 1000,
-		    _ => panic!(),
-		};
-		todo.push(Reverse((energy + e * aoc::manhattan(*p, mv), new_pos)));
-	    }
-	}
+                        // hallway reached, move there
+                        // println!("move to hallway");
+                        moves.push(hp);
+                    }
+                }
+            }
+            // if moves.is_empty() {
+            // 	println!("NO MOVES!");
+            // }
+            for mv in moves {
+                let mut new_pos = pos.clone();
+		new_pos.remove(p);
+		new_pos.insert(*p, *a);
+                // println!("old-pos: {:?}", pos);
+                // println!("new-pos: {:?}", new_pos);
+                let e = match a {
+                    'A' => 1,
+                    'B' => 10,
+                    'C' => 100,
+                    'D' => 1000,
+                    _ => panic!(),
+                };
+                todo.push(Reverse((energy + e * aoc::manhattan(*p, mv), new_pos)));
+            }
+        }
     }
     None
 }
@@ -101,18 +103,18 @@ fn part1(grid: &Parsed) -> Answer {
     let mut gd = aoc::PrintGridDrawer::new(|c| c);
     gd.draw(grid);
     // Hardcoded goals
-    let goals = HashMap::from([
+    let goals = BTreeMap::from([
         ('A', vec![[3, 2], [3, 3]]),
         ('B', vec![[5, 2], [5, 3]]),
         ('C', vec![[7, 2], [7, 3]]),
         ('D', vec![[9, 2], [9, 3]]),
     ]);
     // Starting positions
-    let mut start = vec![];
+    let mut start = BTreeMap::new();
     for p in grid.points() {
         if let Some(x) = grid.get_value(p) {
             if x.is_ascii_alphabetic() {
-                start.push((x, p));
+                start.insert(p, x);
             }
         }
     }

8071afd2163ec8b96e68136918d86b290f44b154
Pär Bohrarper <par.bohrarper@gmail.com>
2022-10-24 08:50:26 +0200

    Use modulo math trick in day 13

--- 2017/13/main.rs
+++ 2017/13/main.rs
@@ -1,6 +1,5 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::iter::*;
-use std::time::Instant;
 
 #[derive(parse_display::Display, parse_display::FromStr, Debug, Clone, PartialEq, Eq, Hash)]
 #[display("{depth}: {range}")]
@@ -13,79 +12,41 @@ type ParsedItem = Scanner;
 type Parsed = Vec<ParsedItem>;
 type Answer = i64;
 
-fn part1(firewall: &[ParsedItem]) -> Answer {
+fn part1(firewall: &Parsed) -> Answer {
     let ranges: HashMap<i64, i64> = firewall.iter().map(|x| (x.depth, x.range)).collect();
-    let mut state = HashMap::new();
     let layers = firewall.iter().map(|x| x.depth).max().unwrap();
-    let mut severity = 0;
-    for layer in 0..=layers {
-        // Check if caught
-        if state.entry(layer).or_insert((0, 1)).0 == 0 {
-            if let Some(range) = ranges.get(&layer) {
-                severity += layer * range;
-            }
-        }
-        // Update scanners
-        for scanner in firewall {
-            let (pos, dir) = state.entry(scanner.depth).or_insert((0, 1));
-            #[allow(clippy::collapsible_else_if)]
-            if *dir > 0 {
-                if *pos + 1 == scanner.range {
-                    *dir = -1;
-                }
+    let ranges: Vec<i64> = (0..=layers)
+        .map(|x| *ranges.get(&x).unwrap_or(&0))
+        .collect();
+    (0..=layers)
+        .map(|ps| {
+            let range = ranges[ps as usize];
+            if range > 0 && (ps % (2 * (range - 1))) == 0 {
+                ps * range
             } else {
-                if *pos == 0 {
-                    *dir = 1;
-                }
+                0
             }
-            *pos += *dir;
-        }
-    }
-    severity
+        })
+        .sum()
 }
 
-fn part2(firewall: &[ParsedItem]) -> Answer {
+fn part2(firewall: &Parsed) -> Answer {
     let ranges: HashMap<i64, i64> = firewall.iter().map(|x| (x.depth, x.range)).collect();
-    let mut state = HashMap::new();
     let layers = firewall.iter().map(|x| x.depth).max().unwrap();
-    let mut picosecond = 0;
-    let mut failed = HashSet::new();
+    let ranges: Vec<i64> = (0..=layers)
+        .map(|x| *ranges.get(&x).unwrap_or(&0))
+        .collect();
+    let mut delay = 0;
     loop {
-        for delay in (picosecond - layers)..=picosecond {
-            if failed.contains(&delay) {
-                continue;
-            }
-            // Check if caught
-            let layer = picosecond - delay;
-            assert!(layer >= 0 && layer <= layers);
-            if state.entry(layer).or_insert((0, 1)).0 == 0 {
-                if let Some(_range) = ranges.get(&layer) {
-                    failed.insert(delay);
-                }
-            }
+        if !(delay..=(delay + layers)).any(|ps| {
+            let range = ranges[(ps - delay) as usize];
+            range > 0 && (ps % (2 * (range - 1))) == 0
+        }) {
+            break;
         }
-        // Update scanners
-        for scanner in firewall {
-            let (pos, dir) = state.entry(scanner.depth).or_insert((0, 1));
-            #[allow(clippy::collapsible_else_if)]
-            if *dir > 0 {
-                if *pos + 1 == scanner.range {
-                    *dir = -1;
-                }
-            } else {
-                if *pos == 0 {
-                    *dir = 1;
-                }
-            }
-            *pos += *dir;
-        }
-        // Check if it succeded
-        let delay = picosecond - layers;
-        if delay >= 0 && !failed.contains(&delay) {
-            return delay;
-        }
-        picosecond += 1;
+        delay += 1;
     }
+    delay
 }
 
 fn parse(lines: &[String]) -> Parsed {
@@ -93,24 +54,7 @@ fn parse(lines: &[String]) -> Parsed {
 }
 
 fn main() {
-    let start_time = Instant::now();
-    let (part, lines) = aoc::read_lines();
-    let io_time = Instant::now();
-    let parsed = parse(&lines);
-    let parse_time = Instant::now();
-    let result = if part == 1 {
-        part1(&parsed)
-    } else {
-        part2(&parsed)
-    };
-    let done_time = Instant::now();
-    println!(
-        "read: {:?}, parse: {:?}, solve: {:?}\n",
-        io_time.duration_since(start_time),
-        parse_time.duration_since(io_time),
-        done_time.duration_since(parse_time)
-    );
-    println!("{}", result);
+    aoc::run_main(parse, part1, part2);
 }
 
 #[cfg(test)]

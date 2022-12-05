3dfcdacf210643e8cb9fd018703fedcf2c34c043
Pär Bohrarper <par.bohrarper@gmail.com>
2022-12-05 10:40:43 +0100

    Add visualization for day5

--- 2022/05/main.rs
+++ 2022/05/main.rs
@@ -11,12 +11,67 @@ struct Move {
 type Parsed = (Vec<VecDeque<char>>, Vec<Move>);
 type Answer = String;
 
+#[cfg(feature = "vis")]
+mod vis {
+    use super::*;
+    use aoc::GridDrawer;
+
+    pub struct Drawer {
+        drawer: Box<aoc::BitmapGridDrawer<fn([u8; 3]) -> [u8; 3], Vec<Vec<[u8; 3]>>, [u8; 3]>>,
+        stacks: Vec<Vec<VecDeque<char>>>,
+    }
+
+    impl Drawer {
+        pub fn new(name: &str) -> Drawer {
+            Drawer {
+                drawer: Box::new(aoc::BitmapGridDrawer::new(|x| x, name)),
+                stacks: vec![],
+            }
+        }
+
+        pub fn draw(&mut self, stacks: &Vec<VecDeque<char>>) {
+            self.stacks.push(stacks.clone());
+        }
+    }
+
+    impl Drop for Drawer {
+        fn drop(&mut self) {
+            let w = self.stacks[0].len();
+            let h = self
+                .stacks
+                .iter()
+                .map(|x| x.iter().map(|x| x.len()).max().unwrap())
+                .max()
+                .unwrap();
+            let xs = aoc::SMALLFONT.glyph_size().0 + 1;
+            let ys = aoc::SMALLFONT.glyph_size().1 + 1;
+            for stacks in &self.stacks {
+                let mut grid = vec![vec![[0, 0, 0]; w * xs as usize]; h * ys as usize];
+                let mut x = 0;
+                for s in stacks {
+                    let mut y = (h - 1) as i64 * ys as i64;
+                    for c in s {
+                        aoc::print_str(&mut grid, &c.to_string(), [x, y], [255, 255, 255]);
+                        y -= ys as i64;
+                    }
+                    x += xs as i64;
+                }
+                self.drawer.draw(&grid);
+            }
+        }
+    }
+}
+
 fn part1(data: &Parsed) -> Answer {
     let mut stacks = data.0.clone();
+    #[cfg(feature = "vis")]
+    let mut drawer = vis::Drawer::new("vis/5/part1");
     for mv in &data.1 {
         for _i in 0..mv.num {
             let c = stacks[mv.from - 1].pop_front().unwrap();
             stacks[mv.to - 1].push_front(c);
+            #[cfg(feature = "vis")]
+            drawer.draw(&stacks);
         }
     }
     stacks.iter().map(|x| x.front().unwrap()).copied().collect()
@@ -24,6 +79,8 @@ fn part1(data: &Parsed) -> Answer {
 
 fn part2(data: &Parsed) -> Answer {
     let mut stacks = data.0.clone();
+    #[cfg(feature = "vis")]
+    let mut drawer = vis::Drawer::new("vis/5/part2");
     for mv in &data.1 {
         let mut tmp = VecDeque::new();
         for _i in 0..mv.num {
@@ -33,6 +90,8 @@ fn part2(data: &Parsed) -> Answer {
         for x in tmp {
             stacks[mv.to - 1].push_front(x);
         }
+        #[cfg(feature = "vis")]
+        drawer.draw(&stacks);
     }
     stacks.iter().map(|x| x.front().unwrap()).copied().collect()
 }

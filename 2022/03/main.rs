9e8aceebb07976a09b6c663017cd0e504831bbbb
Pär Bohrarper <par.bohrarper@gmail.com>
2022-12-06 13:57:05 +0100

    Use AsciiSet on day3

--- 2022/03/main.rs
+++ 2022/03/main.rs
@@ -1,4 +1,5 @@
-use std::{collections::HashSet, iter::*};
+use aoc::AsciiSet;
+use std::iter::*;
 
 type ParsedItem = String;
 type Parsed = Vec<ParsedItem>;
@@ -16,9 +17,9 @@ fn part1(data: &Parsed) -> Answer {
     data.iter()
         .map(|rs| {
             let half = rs.len() / 2;
-            let first: HashSet<char> = rs[0..half].chars().collect();
-            let second: HashSet<char> = rs[half..].chars().collect();
-            prio(*first.intersection(&second).next().unwrap())
+            let first: AsciiSet = rs[0..half].chars().collect();
+            let second: AsciiSet = rs[half..].chars().collect();
+            prio(first.intersection(&second).next().unwrap())
         })
         .sum()
 }
@@ -26,10 +27,12 @@ fn part1(data: &Parsed) -> Answer {
 fn part2(data: &Parsed) -> Answer {
     data.chunks(3)
         .map(|grp| {
-            let a: HashSet<char> = grp[0].chars().collect();
-            let b: HashSet<char> = grp[1].chars().collect();
-            let c: HashSet<char> = grp[2].chars().collect();
-            prio(*(&(&a & &b) & &c).iter().next().unwrap())
+            let mut a: AsciiSet = grp[0].chars().collect();
+            let b: AsciiSet = grp[1].chars().collect();
+            let c: AsciiSet = grp[2].chars().collect();
+            a.intersect_with(&b);
+            a.intersect_with(&c);
+            prio(a.iter().next().unwrap())
         })
         .sum()
 }

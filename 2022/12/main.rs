af4883967e67b23ef41b2ba8cc6ab5a2e228c319
Pär Bohrarper <par.bohrarper@gmail.com>
2022-12-12 09:33:56 +0100

    Add vis

--- 2022/12/main.rs
+++ 2022/12/main.rs
@@ -33,6 +33,87 @@ fn part1(data: &Parsed) -> Answer {
         goal,
     )
     .unwrap();
+
+    #[cfg(feature = "vis")]
+    {
+        let mut window = kiss3d::window::Window::new_with_size("Day 12", 1280, 720);
+
+        window.set_light(kiss3d::light::Light::StickToCamera);
+
+        let path = p.1.iter().copied().rev().collect::<Vec<_>>();
+
+        let hf = |v: char| {
+            (if v == 'E' {
+                b'z'
+            } else if v == 'S' {
+                b'a'
+            } else {
+                v as u8
+            } - b'a') as f32
+        };
+
+        for p in data.points() {
+            let v = data.get_value(p).unwrap();
+            let h = hf(v);
+            let mut c = window.add_cube(1.0, h, 1.0);
+            c.append_translation(&kiss3d::nalgebra::Translation3::new(
+                p[0] as f32,
+                h / 2.0,
+                p[1] as f32,
+            ));
+            if path.contains(&p) {
+                c.set_color(1.0, 1.0, 0.0);
+            } else {
+                c.set_color(0.0, 0.5 + h / 13.0, 0.0);
+            }
+        }
+
+        let mut i = 0;
+        let eye = kiss3d::nalgebra::Point3::new(path[i][0] as f32, 0.7, path[i][1] as f32);
+        let at = kiss3d::nalgebra::Point3::new(path[i + 1][0] as f32, 0.7, path[i + 1][1] as f32);
+        let mut camera = kiss3d::camera::FirstPerson::new(eye, at);
+        let mut frame = 0;
+        while window.render_with_camera(&mut camera) {
+            if i + 2 < path.len() && frame > 500 {
+                if frame % 20 == 0 {
+                    i += 1;
+                }
+                // Step camera
+                let v1 = data.get_value(path[i]).unwrap();
+                let h1 = hf(v1) + 0.7;
+                let v2 = data.get_value(path[i + 1]).unwrap();
+                let h2 = hf(v2) + 0.7;
+                let f = 2;
+                let v3 = data.get_value(path[(i + f).min(path.len() - 1)]).unwrap();
+                let h3 = hf(v3) + 0.7;
+                let v4 = data
+                    .get_value(path[(i + f + 1).min(path.len() - 1)])
+                    .unwrap();
+                let h4 = hf(v4) + 0.7;
+                let x1 = path[i][0] as f32;
+                let y1 = path[i][1] as f32;
+                let x2 = path[i + 1][0] as f32;
+                let y2 = path[i + 1][1] as f32;
+                let x3 = path[(i + f).min(path.len() - 1)][0] as f32;
+                let y3 = path[(i + f).min(path.len() - 1)][1] as f32;
+                let x4 = path[(i + f + 1).min(path.len() - 1)][0] as f32;
+                let y4 = path[(i + f + 1).min(path.len() - 1)][1] as f32;
+                let d = (frame % 20) as f32 / 20.0;
+                let eye = kiss3d::nalgebra::Point3::new(
+                    x1 + d * (x2 - x1),
+                    h1 + d * (h2 - h1),
+                    y1 + d * (y2 - y1),
+                );
+                let at = kiss3d::nalgebra::Point3::new(
+                    x3 + d * (x4 - x3),
+                    h3 + d * (h4 - h3),
+                    y3 + d * (y4 - y3),
+                );
+                camera.look_at(eye, at);
+            }
+            frame += 1;
+        }
+    }
     p.0
 }
 

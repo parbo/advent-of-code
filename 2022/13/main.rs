cb25b67d2d58b1d026d41455b9c46f6bd9a6f9eb
Pär Bohrarper <par.bohrarper@gmail.com>
2022-12-13 08:29:48 +0100

    Use custom parser instead of serde_json

--- 2022/13/main.rs
+++ 2022/13/main.rs
@@ -1,13 +1,64 @@
-use serde_json::Value;
-use std::{cmp::Ordering, iter::*};
+use std::{cmp::Ordering, iter::*, str::FromStr};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Value {
+    List(Vec<Value>),
+    Number(u32),
+}
+
+impl FromStr for Value {
+    type Err = aoc::ParseError;
+    fn from_str(s: &str) -> Result<Value, aoc::ParseError> {
+        let mut stack = vec![];
+        let mut curr = vec![];
+        let mut curr_n = None;
+        for c in s.chars() {
+            match c {
+                '[' => {
+                    stack.push(Value::List(curr));
+                    curr = vec![];
+                }
+                ']' => {
+                    if let Some(n) = curr_n {
+                        curr.push(Value::Number(n));
+                    }
+                    curr_n = None;
+                    if let Some(Value::List(mut v)) = stack.pop() {
+                        v.push(Value::List(curr));
+                        curr = v;
+                    } else {
+                        return Err(aoc::ParseError::Generic);
+                    }
+                }
+                '0'..='9' => {
+                    let n = curr_n.unwrap_or_default() * 10
+                        + c.to_digit(10).ok_or(aoc::ParseError::Generic)?;
+                    curr_n = Some(n);
+                }
+                ',' => {
+                    if let Some(n) = curr_n {
+                        curr.push(Value::Number(n));
+                    }
+                    curr_n = None;
+                }
+                _ => return Err(aoc::ParseError::Generic),
+            }
+        }
+        if !stack.is_empty() || curr_n.is_some() {
+            Err(aoc::ParseError::Generic)
+        } else {
+            Ok(Value::List(curr))
+        }
+    }
+}
 
 type Parsed = Vec<(Value, Value)>;
 type Answer = usize;
 
 fn compare(a: &Value, b: &Value) -> Ordering {
     match (a, b) {
-        (Value::Number(x), Value::Number(y)) => x.as_i64().unwrap().cmp(&y.as_i64().unwrap()),
-        (Value::Array(x), Value::Array(y)) => {
+        (Value::Number(x), Value::Number(y)) => x.cmp(y),
+        (Value::List(x), Value::List(y)) => {
             let mut i = 0;
             for (xx, yy) in zip(x, y) {
                 let r = compare(xx, yy);
@@ -24,9 +75,8 @@ fn compare(a: &Value, b: &Value) -> Ordering {
                 Ordering::Greater
             }
         }
-        (Value::Array(_), Value::Number(_)) => compare(a, &Value::Array(vec![b.clone()])),
-        (Value::Number(_), Value::Array(_)) => compare(&Value::Array(vec![a.clone()]), b),
-        _ => panic!(),
+        (Value::List(_), Value::Number(_)) => compare(a, &Value::List(vec![b.clone()])),
+        (Value::Number(_), Value::List(_)) => compare(&Value::List(vec![a.clone()]), b),
     }
 }
 
@@ -44,8 +94,8 @@ fn part1(data: &Parsed) -> Answer {
 }
 
 fn part2(data: &Parsed) -> Answer {
-    let div1: Value = serde_json::from_str("[[2]]").unwrap();
-    let div2: Value = serde_json::from_str("[[6]]").unwrap();
+    let div1: Value = "[[2]]".parse().unwrap();
+    let div2: Value = "[[6]]".parse().unwrap();
     let mut all = vec![div1.clone(), div2.clone()];
     for (a, b) in data {
         all.push(a.clone());
@@ -61,12 +111,7 @@ fn parse(lines: &[String]) -> Parsed {
     let parts = aoc::split_by_empty_line(lines);
     parts
         .iter()
-        .map(|x| {
-            (
-                serde_json::from_str(x[0]).unwrap(),
-                serde_json::from_str(x[1]).unwrap(),
-            )
-        })
+        .map(|x| (x[0].parse().unwrap(), x[1].parse().unwrap()))
         .collect()
 }
 

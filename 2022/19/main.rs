ea0717d71e640d806a7721e80a7a724252c47e09
Pär Bohrarper <par.bohrarper@gmail.com>
2022-12-19 10:07:25 +0100

    cleanup

--- 2022/19/main.rs
+++ 2022/19/main.rs
@@ -4,15 +4,17 @@ use std::{collections::BinaryHeap, iter::*};
 type ParsedItem = Vec<i64>;
 type Parsed = Vec<ParsedItem>;
 
+const GEODE: usize = 0;
+const OBSIDIAN: usize = 1;
+const CLAY: usize = 2;
+const ORE: usize = 3;
+
 #[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, Hash)]
 struct State {
-    geodes: i64,
+    resources: [i64; 4],
     robots: [i64; 4],
-    obsidian: i64,
-    clay: i64,
-    ore: i64,
-    minute: i64,
     build: [i64; 4],
+    minute: i64,
 }
 
 fn geodes(blueprint: &[i64], time_cap: i64) -> i64 {
@@ -22,90 +24,75 @@ fn geodes(blueprint: &[i64], time_cap: i64) -> i64 {
     frontier.push((
         0,
         State {
-            geodes: 0,
-            obsidian: 0,
-            clay: 0,
-            ore: 0,
+            resources: [0, 0, 0, 0],
             robots: [0, 0, 0, 1],
             build: [0; 4],
             minute: 0,
         },
     ));
-    let ore_robot_cost = (blueprint[1], 0, 0);
-    let clay_robot_cost = (blueprint[2], 0, 0);
-    let obsidian_robot_cost = (blueprint[3], blueprint[4], 0);
-    let geode_robot_cost = (blueprint[5], 0, blueprint[6]);
     let costs = [
-        geode_robot_cost,
-        obsidian_robot_cost,
-        clay_robot_cost,
-        ore_robot_cost,
+        (blueprint[5], 0, blueprint[6]),
+        (blueprint[3], blueprint[4], 0),
+        (blueprint[2], 0, 0),
+        (blueprint[1], 0, 0),
     ];
-    let mut best = 0;
     let max_robots = [
         i64::MAX,
         costs.iter().map(|x| x.2).max().unwrap(),
         costs.iter().map(|x| x.1).max().unwrap(),
         costs.iter().map(|x| x.0).max().unwrap(),
     ];
-
+    let mut best = 0;
     while let Some((e, state)) = frontier.pop() {
         if e < best {
             break;
         }
         if state.minute == time_cap {
-            if state.geodes > best {
-                best = state.geodes;
+            if state.resources[GEODE] > best {
+                best = state.resources[GEODE];
             }
             continue;
         }
         let mut states = vec![state];
         for (i, (ore_cost, clay_cost, obsidian_cost)) in costs.iter().enumerate() {
             let mut ns = state;
-            if *ore_cost <= ns.ore
-                && *clay_cost <= ns.clay
-                && *obsidian_cost <= ns.obsidian
+            if *ore_cost <= ns.resources[ORE]
+                && *clay_cost <= ns.resources[CLAY]
+                && *obsidian_cost <= ns.resources[OBSIDIAN]
                 && ns.build.iter().all(|x| *x == 0)
                 && ns.robots[i] < max_robots[i]
             {
-                ns.ore -= ore_cost;
-                ns.clay -= clay_cost;
-                ns.obsidian -= obsidian_cost;
+                ns.resources[ORE] -= ore_cost;
+                ns.resources[CLAY] -= clay_cost;
+                ns.resources[OBSIDIAN] -= obsidian_cost;
                 ns.build[i] = 1;
                 states.push(ns);
             }
         }
         for mut ns in states {
-            ns.ore += ns.robots[3];
-            ns.clay += ns.robots[2];
-            ns.obsidian += ns.robots[1];
-            ns.geodes += ns.robots[0];
             for i in 0..4 {
+                ns.resources[i] += ns.robots[i];
                 ns.robots[i] += ns.build[i];
             }
             ns.build = [0; 4];
             ns.minute += 1;
             if visited.insert(ns) {
-                let mut ore = ns.ore;
-                let mut clay = ns.clay;
-                let mut obsidian = ns.obsidian;
-                let mut geodes = ns.geodes;
-                let mut gr = ns.robots[0];
+                let mut res = ns.resources;
+                let mut gr = ns.robots[GEODE];
                 for i in 0..(time_cap - ns.minute) {
-                    ore += ns.robots[3] + i;
-                    clay += ns.robots[2] + i;
-                    obsidian += ns.robots[1] + i;
-                    if geode_robot_cost.0 <= ore
-                        && geode_robot_cost.1 <= clay
-                        && geode_robot_cost.2 <= obsidian
+                    #[allow(clippy::needless_range_loop)]
+                    for r in OBSIDIAN..=ORE {
+                        res[r] += ns.robots[r] + i;
+                    }
+                    if costs[GEODE].0 <= res[ORE]
+                        && costs[GEODE].1 <= res[CLAY]
+                        && costs[GEODE].2 <= res[OBSIDIAN]
                     {
                         gr += 1;
                     }
-                    geodes += gr;
+                    res[GEODE] += gr;
                 }
-                frontier.push((geodes, ns));
-            } else {
-                // println!("already visited: {:?}", ns);
+                frontier.push((res[GEODE], ns));
             }
         }
     }

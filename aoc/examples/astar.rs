ef12dfe7c98eb1a6d02189ed1c5110d22f52e4ed
Pär Bohrarper <par.bohrarper@gmail.com>
2022-12-25 13:52:07 +0100

    Fix aoc tests

--- aoc/examples/astar.rs
+++ aoc/examples/astar.rs
@@ -19,10 +19,11 @@ fn make_grid() -> Vec<String> {
 
 fn astar_char() {
     let mut grid = aoc::parse_grid(&make_grid());
-    let graph = aoc::grid_to_graph(&grid, |_p, c| *c == '.', |_p1, _c1, _p2, _c2| Some(1), 4);
+    let graph =
+        aoc::grid_to_undirected_graph(&grid, |_p, c| *c == '.', |_p1, _c1, _p2, _c2| Some(1), 4);
     let a: aoc::Point = [1, 1];
     let f: aoc::Point = [1, 7];
-    if let Some((_cost, path)) = aoc::astar(&graph, a, f) {
+    if let Some((_cost, path)) = aoc::astar_graph(&graph, a, f) {
         for point in &path {
             grid.set_value(*point, '*');
         }
@@ -34,10 +35,11 @@ fn astar_char() {
 fn astar_char_sparse() {
     let mut grid =
         aoc::parse_grid_to_sparse(&make_grid(), |c| if c == '.' { Some(c) } else { None });
-    let graph = aoc::grid_to_graph(&grid, |_p, c| *c == '.', |_p1, _c1, _p2, _c2| Some(1), 4);
+    let graph =
+        aoc::grid_to_undirected_graph(&grid, |_p, c| *c == '.', |_p1, _c1, _p2, _c2| Some(1), 4);
     let a: aoc::Point = [1, 1];
     let f: aoc::Point = [1, 7];
-    if let Some((_cost, path)) = aoc::astar(&graph, a, f) {
+    if let Some((_cost, path)) = aoc::astar_graph(&graph, a, f) {
         for point in &path {
             grid.set_value(*point, '*');
         }

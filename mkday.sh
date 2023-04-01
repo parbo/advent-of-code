b5928592ed01614392506ceee95eb31414beb55f
Pär Bohrarper <par.bohrarper@gmail.com>
2023-04-01 15:36:34 +0200

    Make it so template isn't picked up as a project

--- mkday.sh
+++ mkday.sh
@@ -4,5 +4,6 @@ year=$1
 zpd=$(printf "%02d" $2)
 mkdir -p $year
 cp -R template $year/$zpd
+for f in $year/$zpd/*; do mv $f ${f%".template"}; done
 sed -i.bak "s/template/day$zpd/g" "$year/$zpd/Cargo.toml"
 rm "$year/$zpd/Cargo.toml.bak"

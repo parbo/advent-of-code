15dc53ce8a4121439eb02e1997dc547f8317c4e0
Pär Bohrarper <par.bohrarper@gmail.com>
2023-12-06 06:28:27 +0100

    cleanup

--- 2023/06/main.rs
+++ 2023/06/main.rs
@@ -1,59 +1,25 @@
 use std::iter::*;
 
-// #[derive(parse_display::Display, parse_display::FromStr, Debug, Clone, PartialEq, Eq, Hash)]
-// #[display("{thing}: {al}-{ah} or {bl}-{bh}")]
-// struct Rule {
-//     thing: String,
-//     al: i64,
-//     ah: i64,
-//     bl: i64,
-//     bh: i64,
-// }
-
 type Parsed = Vec<String>;
+
+fn ways_to_win(time: i64, distance: i64) -> i64 {
+    (0..=time).filter(|t| (time - t) * t > distance).count() as i64
+}
 
 fn part1(data: &Parsed) -> i64 {
     let times = aoc::things::<i64>(&data[0]);
     let distances = aoc::things::<i64>(&data[1]);
-    let mut margin = vec![];
-    for (time, distance) in times.into_iter().zip(distances) {
-        let mut n = 0;
-        for t in 0..=time {
-            let time_left = time - t;
-            let d = time_left * t;
-            if d > distance {
-                n += 1;
-            }
-        }
-        margin.push(n);
-    }
-    margin.iter().product()
+    times
+        .into_iter()
+        .zip(distances)
+        .map(|(time, distance)| ways_to_win(time, distance))
+        .product()
 }
 
 fn part2(data: &Parsed) -> i64 {
-    let time = data[0]
-        .split_once(':')
-        .unwrap()
-        .1
-        .replace(' ', "")
-        .parse::<i64>()
-        .unwrap();
-    let distance = data[1]
-        .split_once(':')
-        .unwrap()
-        .1
-        .replace(' ', "")
-        .parse::<i64>()
-        .unwrap();
-    let mut n = 0;
-    for t in 0..=time {
-        let time_left = time - t;
-        let d = time_left * t;
-        if d > distance {
-            n += 1;
-        }
-    }
-    n
+    let time = data[0][10..].replace(' ', "").parse::<i64>().unwrap();
+    let distance = data[1][10..].replace(' ', "").parse::<i64>().unwrap();
+    ways_to_win(time, distance)
 }
 
 fn parse(lines: &[String]) -> Parsed {
@@ -78,5 +44,10 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(part1(&parse(&example())), 288);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&parse(&example())), 71503);
     }
 }

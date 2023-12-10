aa34dcadb583f25b6ac2245c80822343c9d5d3d5
Pär Bohrarper <par.bohrarper@gmail.com>
2023-12-10 09:27:44 +0100

    part2

--- 2023/10/main.rs
+++ 2023/10/main.rs
@@ -1,4 +1,4 @@
-use aoc::{Grid, GridDrawer, EAST, NORTH, SOUTH, WEST};
+use aoc::{Grid, EAST, NORTH, SOUTH, WEST};
 use std::{
     collections::{HashMap, HashSet},
     iter::*,
@@ -6,12 +6,11 @@ use std::{
 
 type Parsed = Vec<Vec<char>>;
 
-fn get_loop(data: &Parsed) -> HashMap<aoc::Point, i64> {
+fn get_loop(data: &Parsed) -> (Vec<aoc::Point>, HashMap<aoc::Point, i64>) {
     let s = data
         .points()
         .find(|p| data.get_value(*p).unwrap() == 'S')
         .unwrap();
-    dbg!(s);
     let mut distances = HashMap::from([(s, 0)]);
     let poss = HashMap::from([
         ('-', vec![EAST, WEST]),
@@ -23,43 +22,126 @@ fn get_loop(data: &Parsed) -> HashMap<aoc::Point, i64> {
         ('S', vec![SOUTH, EAST, WEST, NORTH]),
     ]);
     let poss2 = HashMap::from([
-        (EAST, vec!['-', 'J', '7']),
-        (WEST, vec!['-', 'L', 'F']),
-        (NORTH, vec!['|', 'F', '7']),
-        (SOUTH, vec!['|', 'J', 'L']),
+        (EAST, vec!['-', 'J', '7', 'S']),
+        (WEST, vec!['-', 'L', 'F', 'S']),
+        (NORTH, vec!['|', 'F', '7', 'S']),
+        (SOUTH, vec!['|', 'J', 'L', 'S']),
     ]);
-    let mut frontier = vec![s];
-    while let Some(pos) = frontier.pop() {
+    let mut frontier = vec![vec![s]];
+    let mut paths = vec![];
+    while let Some(path) = frontier.pop() {
+        let pos = *path.last().unwrap();
         let c = data.get_value(pos).unwrap();
         if let Some(nbs) = poss.get(&c) {
             let d = *distances.get(&pos).unwrap_or(&0);
             for nb in nbs {
                 let new_pos = aoc::point_add(pos, *nb);
                 if let Some(new_c) = data.get_value(new_pos) {
-                    if poss2.get(nb).unwrap().contains(&new_c) {
+                    if path.len() > 2 && new_c == 'S' {
+                        paths.push(path.clone());
+                        continue;
+                    }
+                    if !path.contains(&new_pos) && poss2.get(nb).unwrap().contains(&new_c) {
                         let dd = d + 1;
                         let e = distances.entry(new_pos).or_insert(dd);
                         if dd <= *e {
                             *e = dd;
-                            frontier.push(new_pos);
                         }
+                        let mut new_path = path.clone();
+                        new_path.push(new_pos);
+                        frontier.push(new_path);
                     }
                 }
             }
         }
     }
-    distances
+    (paths[0].clone(), distances)
 }
 
 fn part1(data: &Parsed) -> i64 {
-    let distances = get_loop(data);
-    let mut gd = aoc::PrintGridDrawer::new(|_x: i64| '*');
-    gd.draw(&distances);
+    let distances = get_loop(data).1;
     *distances.values().max().unwrap()
 }
 
-fn part2(_: &Parsed) -> i64 {
-    0
+fn part2(data: &Parsed) -> i64 {
+    let lp = get_loop(data).0;
+    let mut data = data.clone();
+    let ([min_x, min_y], [max_x, max_y]) = data.extents();
+    for y in min_y..=max_y {
+        for x in min_x..=max_x {
+            if !lp.contains(&[x, y]) {
+                data[y as usize][x as usize] = '.';
+            }
+        }
+    }
+    let mut right = HashSet::new();
+    let mut left = HashSet::new();
+    for mv in lp.windows(2) {
+        let dir = aoc::point_sub(mv[1], mv[0]);
+        let r = *aoc::DIRECTION_ROTATE_RIGHT.get(&dir).unwrap();
+        let nbr = aoc::point_add(mv[0], r);
+        let nbr2 = aoc::point_add(mv[1], r);
+        if let Some(c) = data.get_value(nbr) {
+            if c == '.' {
+                right.insert(nbr);
+            }
+        }
+        if let Some(c) = data.get_value(nbr2) {
+            if c == '.' {
+                right.insert(nbr2);
+            }
+        }
+        let l = *aoc::DIRECTION_ROTATE_LEFT.get(&dir).unwrap();
+        let nbr = aoc::point_add(mv[0], l);
+        let nbr2 = aoc::point_add(mv[1], l);
+        if let Some(c) = data.get_value(nbr) {
+            if c == '.' {
+                left.insert(nbr);
+            }
+        }
+        if let Some(c) = data.get_value(nbr2) {
+            if c == '.' {
+                left.insert(nbr2);
+            }
+        }
+    }
+    for p in right {
+        data.fill(p, 'r');
+    }
+    for p in left {
+        data.fill(p, 'l');
+    }
+    let ([min_x, min_y], [max_x, max_y]) = data.extents();
+    let mut inside = '.';
+    let edge = (min_x..=max_x)
+        .map(|x| [x, 0])
+        .chain((min_x..=max_x).map(|x| [x, max_y]))
+        .chain((min_y..=max_y).map(|y| [0, y]))
+        .chain((min_y..=max_y).map(|y| [max_x, y]));
+    for p in edge {
+        match data.get_value(p) {
+            Some('l') => {
+                inside = 'r';
+                break;
+            }
+            Some('r') => {
+                inside = 'l';
+                break;
+            }
+            _ => (),
+        }
+    }
+    let unknown = data
+        .points()
+        .filter_map(|x| data.get_value(x))
+        .filter(|x| *x == '.')
+        .count() as i64;
+    assert_eq!(unknown, 0);
+    assert_ne!(inside, '.');
+    data.points()
+        .filter_map(|x| data.get_value(x))
+        .filter(|x| *x == inside)
+        .count() as i64
 }
 
 fn parse(lines: &[String]) -> Parsed {
@@ -84,8 +166,48 @@ mod tests {
         ]
     }
 
+    fn example2() -> Vec<String> {
+        vec![
+            ".F----7F7F7F7F-7....".into(),
+            ".|F--7||||||||FJ....".into(),
+            ".||.FJ||||||||L7....".into(),
+            "FJL7L7LJLJ||LJ.L-7..".into(),
+            "L--J.L7...LJS7F-7L7.".into(),
+            "....F-J..F7FJ|L7L7L7".into(),
+            "....L7.F7||L7|.L7L7|".into(),
+            ".....|FJLJ|FJ|F7|.LJ".into(),
+            "....FJL-7.||.||||...".into(),
+            "....L---J.LJ.LJLJ...".into(),
+        ]
+    }
+
+    fn example3() -> Vec<String> {
+        vec![
+            "FF7FSF7F7F7F7F7F---7".into(),
+            "L|LJ||||||||||||F--J".into(),
+            "FL-7LJLJ||||||LJL-77".into(),
+            "F--JF--7||LJLJ7F7FJ-".into(),
+            "L---JF-JLJ.||-FJLJJ7".into(),
+            "|F|F-JF---7F7-L7L|7|".into(),
+            "|FFJF7L7F-JF7|JL---7".into(),
+            "7-L-JL7||F7|L7F-7F7|".into(),
+            "L.L7LFJ|||||FJL7||LJ".into(),
+            "L7JLJL-JLJLJL--JLJ.L".into(),
+        ]
+    }
+
     #[test]
     fn test_part1() {
         assert_eq!(part1(&parse(&example())), 8);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&parse(&example2())), 8);
+    }
+
+    #[test]
+    fn test_part2_2() {
+        assert_eq!(part2(&parse(&example3())), 10);
     }
 }

9e07315d57119ca92122f17e41b1030574438914
Pär Bohrarper <par.bohrarper@gmail.com>
2023-12-21 22:54:22 +0100

    wip

--- 2023/21/main.rs
+++ 2023/21/main.rs
@@ -4,7 +4,7 @@ use aoc::{Grid, GridDrawer};
 
 type Parsed = Vec<Vec<char>>;
 
-fn solve1(data: &Parsed, steps: i64, extend: bool) -> aoc::FxHashSet<aoc::Point> {
+fn solve(data: &Parsed, steps: i64, extend: bool) -> aoc::FxHashSet<aoc::Point> {
     let p = data
         .points()
         .find(|p| data.get_value(*p) == Some('S'))
@@ -71,6 +71,10 @@ fn solve1(data: &Parsed, steps: i64, extend: bool) -> aoc::FxHashSet<aoc::Point>
             last = s;
             last_r = reachable.len();
 
+            if s == steps as usize {
+                break;
+            }
+
             std::mem::swap(&mut last_r_0, &mut last_r_1);
             std::mem::swap(&mut last_r_1, &mut reachable);
             reachable.clear();
@@ -99,83 +103,6 @@ fn solve1(data: &Parsed, steps: i64, extend: bool) -> aoc::FxHashSet<aoc::Point>
     reachable
 }
 
-fn do_solve(
-    data: &Parsed,
-    orig_steps: i64,
-    steps: i64,
-    orig_p: aoc::Point,
-    p: aoc::Point,
-    extend: bool,
-    cache: &mut aoc::FxHashMap<(aoc::Point, i64), (aoc::Point, aoc::FxHashSet<aoc::Point>)>,
-) -> aoc::FxHashSet<aoc::Point> {
-    let ([min_x, min_y], [max_x, max_y]) = data.extents();
-    let h = max_y - min_y + 1;
-    let w = max_x - min_x + 1;
-    let pk = if extend {
-        [p[0].rem_euclid(w), p[1].rem_euclid(h)]
-    } else {
-        p
-    };
-    if let Some((op, v)) = cache.get(&(pk, steps)) {
-        // Translate the positions back
-        let mut vv = aoc::FxHashSet::default();
-        let diff = aoc::point_sub(p, *op);
-        for vp in v {
-            // assert!(aoc::manhattan(*vp, orig_p) <= orig_steps);
-            let pp = aoc::point_add(*vp, diff);
-            // if aoc::manhattan(pp, orig_p) > orig_steps {
-            //     dbg!(diff, p, pk, steps, vp, pp, orig_p, orig_steps);
-            // }
-            // assert!(aoc::manhattan(pp, orig_p) <= orig_steps);
-            vv.insert(pp);
-        }
-        vv
-        // v.clone()
-    } else if steps == 0 {
-        let mut r = aoc::FxHashSet::default();
-        // if aoc::manhattan(p, orig_p) > orig_steps {
-        //     dbg!(p, orig_steps);
-        // }
-        // assert!(aoc::manhattan(p, orig_p) <= orig_steps);
-        r.insert(p);
-        r
-    } else {
-        let mut v = aoc::FxHashSet::default();
-        for pp in aoc::neighbors(p) {
-            let ppp = if extend {
-                [pp[0].rem_euclid(w), pp[1].rem_euclid(h)]
-            } else {
-                pp
-            };
-            let c = data.get_value(ppp);
-            if c == Some('.') || c == Some('S') {
-                v = v
-                    .union(&do_solve(
-                        data,
-                        orig_steps,
-                        steps - 1,
-                        orig_p,
-                        pp,
-                        extend,
-                        cache,
-                    ))
-                    .cloned()
-                    .collect();
-            }
-        }
-        // if pk == [4, 0] {
-        //     dbg!(p, pk, &v);
-        // }
-        let x = cache.insert((pk, steps), (p, v.clone()));
-        if cache.len() % 10000 == 0 {
-            dbg!(cache.len());
-        }
-        assert!(x.is_none());
-        v
-    }
-    reachable
-}
-
 fn make_g(data: &Parsed, r: &aoc::FxHashSet<aoc::Point>) -> aoc::FxHashMap<aoc::Point, char> {
     let mut g: aoc::FxHashMap<_, _> = r.iter().map(|x| (*x, 'O')).collect();
     let ([min_x, min_y], [max_x, max_y]) = data.extents();
@@ -197,43 +124,14 @@ fn make_g(data: &Parsed, r: &aoc::FxHashSet<aoc::Point>) -> aoc::FxHashMap<aoc::
     g
 }
 
-fn solve(data: &Parsed, steps: i64, extend: bool) -> i64 {
-    let p = data
-        .points()
-        .find(|p| data.get_value(*p) == Some('S'))
-        .unwrap();
-    let mut cache = aoc::FxHashMap::default();
-    // let mut gd = aoc::PrintGridDrawer::new(|c| c);
-    let mut gd = aoc::BitmapGridDrawer::new(
-        |c| match c {
-            '#' => [0, 255, 0],
-            'O' => [255, 255, 255],
-            _ => [0, 0, 0],
-        },
-        "vis/21/part2",
-    );
-    gd.set_bg([0, 0, 0]);
-    let mut last = 0;
-    for s in 1..300 {
-        let r = do_solve(data, s, s, p, p, extend, &mut cache);
-        let g = make_g(data, &r);
-        gd.draw(&g);
-        println!();
-        println!("steps: {}, num: {}, inc: {}", s, r.len(), r.len() - last);
-        println!();
-        last = r.len();
-    }
-    //    r.len() as i64
-    0
-}
-
 fn part1(data: &Parsed) -> i64 {
-    solve(data, 64, false)
+    let r = solve(data, 64, false);
+    r.len() as i64
 }
 
 fn part2(data: &Parsed) -> i64 {
-    let r = solve1(data, 26501365, true);
-    return r.len() as i64;
+    let r = solve(data, 26501365, true);
+    r.len() as i64
     // //    solve(data, 26501365, true)
     // let mut gd = aoc::BitmapGridDrawer::new(
     //     |c| match c {
@@ -279,12 +177,12 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(solve(&parse(&example()), 6, true), 16);
-        assert_eq!(solve(&parse(&example()), 10, true), 50);
-        assert_eq!(solve(&parse(&example()), 50, true), 1594);
-        assert_eq!(solve(&parse(&example()), 100, true), 6536);
-        assert_eq!(solve(&parse(&example()), 500, true), 167004);
-        assert_eq!(solve(&parse(&example()), 1000, true), 668697);
-        assert_eq!(solve(&parse(&example()), 5000, true), 16733044);
+        assert_eq!(solve(&parse(&example()), 6, true).len(), 16);
+        assert_eq!(solve(&parse(&example()), 10, true).len(), 50);
+        assert_eq!(solve(&parse(&example()), 50, true).len(), 1594);
+        assert_eq!(solve(&parse(&example()), 100, true).len(), 6536);
+        assert_eq!(solve(&parse(&example()), 500, true).len(), 167004);
+        // assert_eq!(solve(&parse(&example()), 1000, true).len(), 668697);
+        // assert_eq!(solve(&parse(&example()), 5000, true).len(), 16733044);
     }
 }

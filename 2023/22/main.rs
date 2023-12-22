325f1e64e24dd5a05e0b95c88f37cf70c0ead23d
Pär Bohrarper <par.bohrarper@gmail.com>
2023-12-22 16:51:37 +0100

    vis

--- 2023/22/main.rs
+++ 2023/22/main.rs
@@ -1,71 +1,103 @@
-use std::iter::*;
+use std::{iter::*, path::PathBuf};
 
-use aoc::{FxHashMap, Vec3};
+use aoc::Vec3;
 
 type ParsedItem = (Vec3, Vec3);
 type Parsed = Vec<ParsedItem>;
 
-fn draw(data: &Parsed) {
-    let mut window = kiss3d::window::Window::new_with_size("Day 12", 1280, 720);
+struct Drawer {
+    window: kiss3d::window::Window,
+    camera: kiss3d::camera::ArcBall,
+    cubes: Vec<kiss3d::scene::SceneNode>,
+    data: Vec<Parsed>,
+}
 
-    for (s, e) in data {
-        let mut c = window.add_cube(
-            (e[1] - s[1] + 1) as f32,
-            (e[0] - s[0] + 1) as f32,
-            (e[2] - s[2] + 1) as f32,
-        );
-        c.set_color(1.0, 1.0, 0.0);
-        c.append_translation(&kiss3d::nalgebra::Translation3::new(
-            s[1] as f32,
-            s[0] as f32,
-            s[2] as f32,
-        ));
+impl Drawer {
+    fn new(data: Vec<Parsed>) -> Self {
+        let mut window = kiss3d::window::Window::new_with_size("Day 22", 100, 1200);
+        window.set_light(kiss3d::light::Light::StickToCamera);
+        let eye = kiss3d::nalgebra::Point3::new(0.0, 400.0, 150.0);
+        let at = kiss3d::nalgebra::Point3::new(0.0, 0.0, 150.0);
+        // let at = kiss3d::nalgebra::Point3::origin();
+        let mut camera = kiss3d::camera::ArcBall::new(eye, at);
+        camera.set_up_axis(kiss3d::nalgebra::Vector3::new(0.0, 0.0, 1.0));
+        let cubes = vec![];
+        Drawer {
+            window,
+            camera,
+            cubes,
+            data,
+        }
     }
 
-    window.set_light(kiss3d::light::Light::StickToCamera);
-    let eye = kiss3d::nalgebra::Point3::new(0.0, 0.0, 80.0);
-    let at = kiss3d::nalgebra::Point3::origin();
-    let mut camera = kiss3d::camera::ArcBall::new(eye, at);
-    // let mut camera = kiss3d::camera::FirstPerson::new(eye, at);
-    // let mut frame = 0;
-    // let png_path = PathBuf::from("vis/12/part1");
-    // if let Some(parent) = png_path.parent() {
-    //     std::fs::create_dir_all(parent).expect("could not create folder");
-    // }
-    while window.render_with_camera(&mut camera) {
-        // rotate the arc-ball camera.
-        // let curr_yaw = camera.yaw();
-        // camera.set_yaw(curr_yaw + 0.01);
+    fn draw(&mut self, ix: usize) {
+        for c in &mut self.cubes {
+            self.window.remove_node(c);
+        }
+        self.cubes.clear();
+        for (s, e) in &self.data[ix] {
+            let mut c = self.window.add_cube(
+                (e[1] - s[1] + 1) as f32,
+                (e[0] - s[0] + 1) as f32,
+                (e[2] - s[2] + 1) as f32,
+            );
+            c.set_color(1.0, 1.0, 0.0);
+            c.append_translation(&kiss3d::nalgebra::Translation3::new(
+                s[1] as f32,
+                s[0] as f32,
+                s[2] as f32,
+            ));
+            self.cubes.push(c);
+        }
     }
-    // // Save image
-    // let filename = png_path.parent().unwrap().join(&format!(
-    //     "{}_{:06}.png",
-    //     png_path.file_name().unwrap().to_str().unwrap(),
-    //     frame
-    // ));
-    // let img = window.snap_image();
-    // img.save(filename).unwrap();
-    // frame += 1;
-    // dbg!(&data);
+
+    fn render(&mut self) {
+        let mut frame = 0;
+        let mut ix = 0;
+        let png_path = PathBuf::from("vis/22/part1");
+        if let Some(parent) = png_path.parent() {
+            std::fs::create_dir_all(parent).expect("could not create folder");
+        }
+        while self.window.render_with_camera(&mut self.camera) {
+            println!("{}/{}", frame, self.data.len());
+            self.draw(ix);
+            // Save image
+            let filename = png_path.parent().unwrap().join(&format!(
+                "{}_{:06}.png",
+                png_path.file_name().unwrap().to_str().unwrap(),
+                frame
+            ));
+            let img = self.window.snap_image();
+            img.save(filename).unwrap();
+            frame += 1;
+            ix += 100;
+            if ix > self.data.len() {
+                break;
+            }
+            let curr_yaw = self.camera.yaw();
+            self.camera.set_yaw(curr_yaw + 0.05);
+            // dbg!(&data);
+        }
+    }
 }
 
 #[allow(clippy::needless_range_loop)]
-fn collapse(data: &Parsed) -> (Vec<(Vec3, Vec3)>, usize) {
-    let mut data: Vec<_> = data.iter().cloned().enumerate().collect();
+fn collapse(data: &Parsed, mut drawer: impl FnMut(&Parsed)) -> (Vec<(Vec3, Vec3)>, usize) {
+    let mut data = data.to_vec();
     // sort on descending z
-    data.sort_by(|a, b| b.1 .0[2].cmp(&a.1 .0[2]));
+    data.sort_by(|a, b| b.0[2].cmp(&a.0[2]));
     // fall down
     let n = data.len();
     let mut num = 0;
     for i in (0..n).rev() {
         // dbg!(i);
-        let (_dix, d) = data[i];
+        let d = data[i];
         let istart = [d.0[0], d.0[1]];
         let iend = [d.1[0], d.1[1]];
         let mut max_z = 0;
         // let mut candidates = vec![];
         for j in (i + 1)..n {
-            let (_djix, dj) = data[j];
+            let dj = data[j];
             let jstart = [dj.0[0], dj.0[1]];
             let jend = [dj.1[0], dj.1[1]];
             let min_xx = istart[0].max(jstart[0]);
@@ -82,19 +114,23 @@ fn collapse(data: &Parsed) -> (Vec<(Vec3, Vec3)>, usize) {
 
         let diff = d.0[2] - max_z;
         if diff > 0 {
-            let (_, d) = &mut data[i];
-            d.0[2] -= diff;
-            d.1[2] -= diff;
+            for _ in 0..diff {
+                {
+                    let d = &mut data[i];
+                    d.0[2] -= 1;
+                    d.1[2] -= 1;
+                }
+                drawer(&data);
+            }
             num += 1;
         }
     }
-    // sort back to original order
-    data.sort_by(|a, b| a.0.cmp(&b.0));
-    (data.into_iter().map(|(_, d)| d).collect(), num)
+    (data, num)
 }
 
 fn part1(data: &Parsed) -> i64 {
-    let (data, _) = collapse(data);
+    let mut vis = vec![];
+    let (data, _) = collapse(data, |x| vis.push(x.clone()));
 
     let mut actually_safe = vec![];
     for s in 0..data.len() {
@@ -104,17 +140,20 @@ fn part1(data: &Parsed) -> i64 {
             .filter_map(|(i, d)| if i == s { None } else { Some(*d) })
             .collect();
 
-        let nd = collapse(&dd);
+        let nd = collapse(&dd, |_| {});
         if nd.0 == dd {
             actually_safe.push(s);
         }
     }
 
+    let mut d = Drawer::new(vis);
+    d.render();
+
     actually_safe.len() as i64
 }
 
 fn part2(data: &Parsed) -> i64 {
-    let (data, _) = collapse(data);
+    let (data, _) = collapse(data, |_| {});
 
     let mut collapsed = 0;
     for s in 0..data.len() {
@@ -124,7 +163,7 @@ fn part2(data: &Parsed) -> i64 {
             .filter_map(|(i, d)| if i == s { None } else { Some(*d) })
             .collect();
 
-        let nd = collapse(&dd);
+        let nd = collapse(&dd, |_| {});
         if nd.0 != dd {
             collapsed += nd.1
         }

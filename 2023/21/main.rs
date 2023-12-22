7898d653a999911f518197f35e6bcd9cdc409634
Pär Bohrarper <par.bohrarper@gmail.com>
2023-12-22 08:25:37 +0100

    meh

--- 2023/21/main.rs
+++ 2023/21/main.rs
@@ -4,7 +4,7 @@ use aoc::{Grid, GridDrawer};
 
 type Parsed = Vec<Vec<char>>;
 
-fn solve(data: &Parsed, steps: i64, extend: bool) -> i64 {
+fn solve(data: &Parsed, steps: i64) -> i64 {
     let p = data
         .points()
         .find(|p| data.get_value(*p) == Some('S'))
@@ -15,46 +15,44 @@ fn solve(data: &Parsed, steps: i64, extend: bool) -> i64 {
     // dbg!(w, h);
     let mut reachable: aoc::FxHashMap<aoc::Point, i64> = aoc::FxHashMap::default();
     reachable.insert(p, 1);
-    let mut gd = aoc::PrintGridDrawer::new(|x: i64| {
-        if let Some(c) = char::from_digit(x as u32, 16) {
-            c
-        } else {
-            '!'
-        }
-    });
-    for _ in 0..steps {
+    // let mut gd = aoc::PrintGridDrawer::new(|x: i64| {
+    //     if let Some(c) = char::from_digit(x as u32, 16) {
+    //         c
+    //     } else {
+    //         '!'
+    //     }
+    // });
+    for s in 0..steps {
         let mut seen = aoc::FxHashSet::default();
         let mut new_reachable = aoc::FxHashMap::default();
         for (p, num) in &reachable {
             for pp in aoc::neighbors(*p) {
-                let ppp = if extend {
-                    [pp[0].rem_euclid(w), pp[1].rem_euclid(h)]
-                } else {
-                    pp
-                };
+                let ppp = [pp[0].rem_euclid(w), pp[1].rem_euclid(h)];
                 // let same = ppp == pp;
                 if seen.insert(pp) {
                     let c = data.get_value(ppp);
                     if c == Some('.') || c == Some('S') {
-                        let e = new_reachable.entry(ppp).or_default();
+                        // let e = new_reachable.entry(ppp).or_default();
+                        let e = new_reachable.entry(pp).or_default();
                         *e += num;
                     }
                 }
             }
         }
         reachable = new_reachable;
-        println!("{}, {:?}", reachable.values().sum::<i64>(), reachable);
-        gd.draw(&reachable);
+        println!("{}, {}", s, reachable.values().sum::<i64>());
+        // println!("{}, {:?}", reachable.values().sum::<i64>(), reachable);
+        // gd.draw(&reachable);
     }
     reachable.values().sum::<i64>()
 }
 
 fn part1(data: &Parsed) -> i64 {
-    solve(data, 64, false)
+    solve(data, 64)
 }
 
 fn part2(data: &Parsed) -> i64 {
-    solve(data, 26501365, true)
+    solve(data, 26501365)
 }
 
 fn parse(lines: &[String]) -> Parsed {

d0d46acbbf0d24685e896b0b1e3485b25792a84a
Pär Bohrarper <par.bohrarper@gmail.com>
2023-12-25 12:45:24 +0100

    wip

--- 2023/25/main.rs
+++ 2023/25/main.rs
@@ -1,6 +1,10 @@
-use std::{collections::BTreeSet, iter::*};
+use std::{
+    cmp::Reverse,
+    collections::{BTreeSet, BinaryHeap},
+    iter::*,
+};
 
-use aoc::{FxHashMap, FxHashSet};
+use aoc::{FxHashMap, FxHashSet, Itertools};
 
 type Parsed = FxHashMap<String, Vec<String>>;
 
@@ -56,6 +60,37 @@ fn nodes(g: &Parsed) -> BTreeSet<&str> {
     c
 }
 
+pub fn dijkstra<'a>(g: &'a Parsed, start: &'a str, goal: &'a str) -> Option<(i64, Vec<&'a str>)> {
+    let mut frontier = BinaryHeap::new();
+    let mut visited: FxHashSet<&str> = FxHashSet::default();
+    let mut came_from = FxHashMap::default();
+    frontier.push(Reverse((0, start)));
+    while let Some(Reverse((score, current))) = frontier.pop() {
+        if visited.contains(&current) {
+            continue;
+        }
+        if current == goal {
+            let mut path = vec![current];
+            let mut curr = current;
+            while curr != start {
+                curr = came_from[&curr];
+                path.push(curr)
+            }
+            return Some((score, path.into_iter().rev().collect()));
+        }
+        for nb in connected(g, current) {
+            if visited.contains(nb) {
+                continue;
+            }
+            let new_score = score + 1;
+            came_from.insert(nb, current);
+            frontier.push(Reverse((new_score, nb)));
+        }
+        visited.insert(current);
+    }
+    None
+}
+
 fn solve(data: &Parsed, rr: &[(&str, &str)]) -> Option<i64> {
     let mut n = nodes(data);
     let num = n.len();
@@ -79,6 +114,29 @@ fn solve(data: &Parsed, rr: &[(&str, &str)]) -> Option<i64> {
 }
 
 fn part1(data: &Parsed) -> i64 {
+    let n = nodes(data);
+    let pairs: Vec<Vec<&str>> = n.into_iter().combinations(2).collect();
+    let pairs: Vec<Vec<&str>> = pairs
+        .into_iter()
+        .filter(|x| {
+            let empty = vec![];
+            data.get(x[0]).unwrap_or(&empty).contains(&x[1].to_string())
+                || data.get(x[1]).unwrap_or(&empty).contains(&x[0].to_string())
+        })
+        .collect();
+    let paths: Vec<Vec<&str>> = pairs
+        .iter()
+        .map(|p| dijkstra(data, p[0], p[1]).unwrap().1)
+        .collect();
+    let mut common: FxHashMap<&str, i64> = FxHashMap::default();
+    for p in &paths {
+        for &s in p {
+            *common.entry(s).or_default() += 1;
+        }
+    }
+    let mut common = common.into_iter().map(|x| x).collect::<Vec<_>>();
+    common.sort_by(|a, b| b.1.cmp(&a.1));
+    dbg!(common);
     // Identified by running graphviz neato engine on input
     let rr = vec![("fvm", "ccp"), ("lhg", "llm"), ("thx", "frl")];
     solve(data, &rr).unwrap()
@@ -119,6 +177,19 @@ mod tests {
                     || data.get(x[1]).unwrap_or(&empty).contains(&x[0].to_string())
             })
             .collect();
+        let paths: Vec<Vec<&str>> = pairs
+            .iter()
+            .map(|p| dijkstra(data, p[0], p[1]).unwrap().1)
+            .collect();
+        let mut common: FxHashMap<&str, i64> = FxHashMap::default();
+        for p in &paths {
+            for &s in p {
+                *common.entry(s).or_default() += 1;
+            }
+        }
+        let mut common = common.into_iter().map(|x| x).collect::<Vec<_>>();
+        common.sort_by(|a, b| b.1.cmp(&a.1));
+        dbg!(common);
         pairs
             .iter()
             .combinations(3)

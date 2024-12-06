d4b31ffe04f12305eaa1d5bff76dc0ad25028fd8
Pär Bohrarper <par.bohrarper@gmail.com>
2024-12-06 13:16:54 -0500

    parallelize with rayon

--- 2024/06/main.rs
+++ 2024/06/main.rs
@@ -1,6 +1,6 @@
-use std::iter::*;
-
 use aoc::Grid;
+use rayon::prelude::*;
+use std::iter::*;
 
 type ParsedItem = Vec<char>;
 type Parsed = Vec<ParsedItem>;
@@ -25,31 +25,33 @@ fn part1(data: &Parsed) -> i64 {
 }
 
 fn part2(data: &Parsed) -> i64 {
-    let mut num = 0;
-    for pp in data.points() {
-        let mut g = data.clone();
-        g.set_value(pp, '#');
-        let mut pos = data
-            .points()
-            .find(|x| data.get_value(*x) == Some('^'))
-            .unwrap();
-        let mut dir = aoc::NORTH;
-        let mut visited = aoc::FxHashSet::default();
-        visited.insert((pos, dir));
-        while let Some(v) = g.get_value(aoc::point_add(pos, dir)) {
-            if v == '#' {
-                dir = *aoc::DIRECTION_ROTATE_RIGHT.get(&dir).unwrap();
-            } else {
-                pos = aoc::point_add(pos, dir);
-                let inserted = visited.insert((pos, dir));
-                if !inserted {
-                    num += 1;
-                    break;
+    let points: Vec<_> = data.points().collect();
+    points
+        .par_iter()
+        .map(|pp| {
+            let mut g = data.clone();
+            g.set_value(*pp, '#');
+            let mut pos = data
+                .points()
+                .find(|x| data.get_value(*x) == Some('^'))
+                .unwrap();
+            let mut dir = aoc::NORTH;
+            let mut visited = aoc::FxHashSet::default();
+            visited.insert((pos, dir));
+            while let Some(v) = g.get_value(aoc::point_add(pos, dir)) {
+                if v == '#' {
+                    dir = *aoc::DIRECTION_ROTATE_RIGHT.get(&dir).unwrap();
+                } else {
+                    pos = aoc::point_add(pos, dir);
+                    let inserted = visited.insert((pos, dir));
+                    if !inserted {
+                        return 1;
+                    }
                 }
             }
-        }
-    }
-    num
+            0
+        })
+        .sum()
 }
 
 fn parse(lines: &[String]) -> Parsed {

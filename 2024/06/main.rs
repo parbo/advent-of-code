09ad41ac87d5fad831954b8e6c56e9a6ab3c4f81
Pär Bohrarper <par.bohrarper@gmail.com>
2024-12-06 07:47:40 -0500

    cleanup

--- 2024/06/main.rs
+++ 2024/06/main.rs
@@ -2,16 +2,6 @@ use std::iter::*;
 
 use aoc::Grid;
 
-// #[derive(parse_display::Display, parse_display::FromStr, Debug, Clone, PartialEq, Eq, Hash)]
-// #[display("{thing}: {al}-{ah} or {bl}-{bh}")]
-// struct Rule {
-//     thing: String,
-//     al: i64,
-//     ah: i64,
-//     bl: i64,
-//     bh: i64,
-// }
-
 type ParsedItem = Vec<char>;
 type Parsed = Vec<ParsedItem>;
 
@@ -68,25 +58,4 @@ fn parse(lines: &[String]) -> Parsed {
 
 fn main() {
     aoc::run_main(parse, part1, part2);
-}
-
-#[cfg(test)]
-mod tests {
-    // use super::*;
-
-    // fn example() -> Vec<String> {
-    //     let s = include_str!("example.txt");
-    //     s.lines().map(|x| x.to_string()).collect()
-    // }
-
-    // fn example() -> Vec<String> {
-    // 	   vec![
-    //         "0".into()
-    //     ]
-    // }
-
-    // #[test]
-    // fn test_part1() {
-    //     assert_eq!(part1(&parse(&example())), 0);
-    // }
 }

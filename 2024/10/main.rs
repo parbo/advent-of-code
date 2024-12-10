cc086d98d171f3cbf6ce3b986e0fc955017f51b4
Pär Bohrarper <par.bohrarper@gmail.com>
2024-12-10 07:11:53 +0100

    add vis

--- 2024/10/main.rs
+++ 2024/10/main.rs
@@ -1,6 +1,5 @@
+use aoc::{Grid, GridDrawer};
 use std::{collections::VecDeque, iter::*};
-
-use aoc::Grid;
 
 type ParsedItem = Vec<char>;
 type Parsed = Vec<ParsedItem>;
@@ -28,10 +27,15 @@ fn search(g: &Parsed, start: aoc::Point) -> i64 {
     found.len() as i64
 }
 
-fn search_paths(g: &Parsed, start: aoc::Point) -> i64 {
+fn search_paths<F: Fn(char) -> Vec<[u8; 3]>>(
+    g: &Parsed,
+    start: aoc::Point,
+    draw: &mut Option<aoc::BitmapSpriteGridDrawer<F, Parsed, char>>,
+) -> i64 {
     let mut todo = VecDeque::from([(vec![start], '0')]);
     let mut found = aoc::FxHashSet::default();
     let mut seen = aoc::FxHashSet::default();
+    let mut all_p = vec![];
     while let Some((p, v)) = todo.pop_back() {
         if v == '9' {
             found.insert(p.clone());
@@ -44,10 +48,23 @@ fn search_paths(g: &Parsed, start: aoc::Point) -> i64 {
             if let Some(value) = g.get_value(nb) {
                 if value as i8 - v as i8 == 1 {
                     let mut newp = p.clone();
+                    if draw.is_some() {
+                        all_p.push(nb);
+                    }
                     newp.push(nb);
                     todo.push_front((newp, value));
                 }
             }
+        }
+    }
+    if let Some(gd) = draw {
+        let ext = g.extents();
+        gd.set_rect(ext);
+        let mut gg = g.clone();
+        for p in all_p {
+            gg.set_value(p, '#');
+            gd.draw(&gg);
+            gd.save_image();
         }
     }
     found.len() as i64
@@ -61,9 +78,28 @@ fn part1(data: &Parsed) -> i64 {
 }
 
 fn part2(data: &Parsed) -> i64 {
+    let mut draw = if cfg!(feature = "vis") {
+        let gd = aoc::BitmapSpriteGridDrawer::new(
+            (2, 2),
+            |c| {
+                if c == '#' {
+                    let v = [0xff, 0, 0];
+                    vec![v, v, v, v]
+                } else {
+                    let v = c as i32 - '0' as i32;
+                    let v = [0, (v * 255 / 9) as u8, 0];
+                    vec![v, v, v, v]
+                }
+            },
+            "vis/day20",
+        );
+        Some(gd)
+    } else {
+        None
+    };
     data.points()
         .filter(|p| data.get_value(*p) == Some('0'))
-        .map(|p| search_paths(data, p))
+        .map(|p| search_paths(data, p, &mut draw))
         .sum()
 }
 

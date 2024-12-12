e7b24f24202bb685c4af2c4d74b33a038db28a67
Pär Bohrarper <par.bohrarper@gmail.com>
2024-12-12 08:51:07 +0100

    vis

--- 2024/12/main.rs
+++ 2024/12/main.rs
@@ -1,6 +1,7 @@
 use std::{collections::BTreeSet, iter::*};
 
 use aoc::Grid;
+use aoc::GridDrawer;
 
 type Parsed = Vec<Vec<char>>;
 
@@ -40,10 +41,36 @@ fn part2(data: &Parsed) -> i64 {
     let crops: BTreeSet<_> = data.points().filter_map(|p| data.get_value(p)).collect();
     let mut cost = 0;
     let mut g = data.clone();
+    #[cfg(feature = "vis")]
+    let mut gd = aoc::BitmapSpriteGridDrawer::new(
+        (
+            aoc::SMALLFONT.glyph_size().0 as i64,
+            aoc::SMALLFONT.glyph_size().1 as i64,
+        ),
+        |x| {
+            let mut v = vec![];
+            let g = aoc::SMALLFONT.glyph(x as u32).unwrap();
+            for y in 0..aoc::SMALLFONT.glyph_size().1 {
+                for x in 0..aoc::SMALLFONT.glyph_size().0 {
+                    let byte = y + (x / 8);
+                    let bit = 7 - (x % 8);
+                    if (g[byte as usize] & (1 << bit)) == 0 {
+                        v.push([0xff, 0xff, 0xff]);
+                    } else {
+                        v.push([0x0, 0x0, 0x0]);
+                    }
+                }
+            }
+            v
+        },
+        "vis/day12",
+    );
     for c in crops {
         let ext = data.extents();
         while let Some(p) = g.points().find(|p| g.get_value(*p) == Some(c)) {
             g.fill(p, '.');
+            #[cfg(feature = "vis")]
+            gd.draw(&g);
             let mut area = 0;
             let mut perimeter = BTreeSet::new();
             for y in (ext.0[1] - 1)..=(ext.1[1] + 1) {
@@ -80,6 +107,8 @@ fn part2(data: &Parsed) -> i64 {
                 }
             }
             g.fill(p, ' ');
+            #[cfg(feature = "vis")]
+            gd.draw(&g);
             cost += area * sides;
         }
     }

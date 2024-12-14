a6a28354e34a6dafb87f59c2624a7a02d7d74030
Pär Bohrarper <par.bohrarper@gmail.com>
2024-12-14 07:28:30 +0100

    cleanup

--- 2024/14/main.rs
+++ 2024/14/main.rs
@@ -1,60 +1,15 @@
 use aoc::GridDrawer;
-use std::{collections::BTreeMap, iter::*};
+use std::iter::*;
 
 type ParsedItem = (aoc::Point, aoc::Point);
 type Parsed = Vec<ParsedItem>;
 
+#[allow(clippy::comparison_chain)]
 fn solve(data: &Parsed, w: i64, h: i64, n: i64) -> i64 {
     let mut robots = data.clone();
-    let mut gd = aoc::BitmapGridDrawer::new(
-        |x: i64| {
-            if x == 0 {
-                [0, 0, 0]
-            } else {
-                [0, 0xff, 0]
-            }
-        },
-        "vis/14/day14",
-    );
-    gd.set_bg([0, 0, 0]);
-    let mut gdc = aoc::PrintGridDrawer::new(|x: i64| if x == 0 { ' ' } else { '*' });
-    let mut seen = aoc::FxHashSet::default();
-    for i in 0..n {
-        for (p, v) in &mut robots {
-            p[0] = (p[0] + v[0]).rem_euclid(w);
-            p[1] = (p[1] + v[1]).rem_euclid(h);
-        }
-        if !seen.insert(robots.clone()) {
-            println!("repeat at {}", i);
-            return 0;
-        }
-        let mut g = vec![vec![0; w as usize]; h as usize];
-        for (p, _v) in &robots {
-            g[p[1] as usize][p[0] as usize] = 1;
-        }
-        let mut numc = 0;
-        for r in &g {
-            let mut n = 0;
-            let mut maxn = 0;
-            let mut last = 0;
-            for c in r {
-                if *c > 0 && *c == last {
-                    n += 1;
-                    maxn = maxn.max(n)
-                } else {
-                    n = 0;
-                }
-                last = *c;
-            }
-            if maxn > 7 {
-                numc += 1;
-            }
-        }
-        if numc > 7 {
-            gd.draw(&g);
-            println!("Step {}", i);
-            gdc.draw(&g);
-        }
+    for (p, v) in &mut robots {
+        p[0] = (p[0] + n * v[0]).rem_euclid(w);
+        p[1] = (p[1] + n * v[1]).rem_euclid(h);
     }
     let mut q = [0, 0, 0, 0];
     for (p, _v) in &robots {
@@ -80,7 +35,56 @@ fn part1(data: &Parsed) -> i64 {
 }
 
 fn part2(data: &Parsed) -> i64 {
-    solve(data, 101, 103, 101 * 103)
+    let w = 101;
+    let h = 103;
+    let n = w * h;
+    let mut robots = data.clone();
+    let mut gd = aoc::BitmapGridDrawer::new(
+        |x: i64| {
+            if x == 0 {
+                [0, 0, 0]
+            } else {
+                [0, 0xff, 0]
+            }
+        },
+        "vis/14/day14",
+    );
+    gd.set_bg([0, 0, 0]);
+    let mut gdc = aoc::PrintGridDrawer::new(|x: i64| if x == 0 { ' ' } else { '*' });
+    for i in 0..n {
+        for (p, v) in &mut robots {
+            p[0] = (p[0] + v[0]).rem_euclid(w);
+            p[1] = (p[1] + v[1]).rem_euclid(h);
+        }
+        let mut g = vec![vec![0; w as usize]; h as usize];
+        for (p, _v) in &robots {
+            g[p[1] as usize][p[0] as usize] = 1;
+        }
+        let mut numc = 0;
+        for r in &g {
+            let mut n = 0;
+            let mut maxn = 0;
+            let mut last = 0;
+            for c in r {
+                if *c > 0 && *c == last {
+                    n += 1;
+                    maxn = maxn.max(n)
+                } else {
+                    n = 0;
+                }
+                last = *c;
+            }
+            if maxn > 7 {
+                numc += 1;
+            }
+        }
+        if numc > 7 {
+            gd.draw(&g);
+            gdc.draw(&g);
+            return i + 1;
+        }
+    }
+    -1
 }
 
 fn parse(lines: &[String]) -> Parsed {

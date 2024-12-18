77754fdb706abd02650b9ba51bc33a7a683622ab
Pär Bohrarper <par.bohrarper@gmail.com>
2024-12-18 06:33:38 +0100

    2024 day 18

---
+++ 2024/18/main.rs
@@ -0,0 +1,86 @@
+use std::iter::*;
+
+use aoc::Grid;
+
+#[cfg(feature = "vis")]
+use aoc::GridDrawer;
+
+type ParsedItem = aoc::Point;
+type Parsed = Vec<ParsedItem>;
+
+fn solve(data: &Parsed, num: usize, w: usize, h: usize) -> Option<(i64, Vec<aoc::Point>)> {
+    let mut g = vec![vec!['.'; w + 1]; h + 1];
+    for p in data.iter().take(num) {
+        g.set_value(*p, '#');
+    }
+    let a: aoc::Point = [0, 0];
+    let f: aoc::Point = [w as i64, h as i64];
+
+    aoc::astar_grid(&g, |_p, c| *c != '#', |_p1, _c1, _p2, _c2| Some(1), a, f)
+}
+
+fn part1(data: &Parsed) -> i64 {
+    solve(data, 1024, 70, 70).unwrap().0
+}
+
+fn part2(data: &Parsed) -> i64 {
+    #[cfg(feature = "vis")]
+    let mut gd = aoc::make_bitmap_text_grid_drawer(
+        |x| {
+            if x == '#' {
+                (x, [0xff, 0xff, 0xff])
+            } else {
+                (x, [0, 0xff, 0])
+            }
+        },
+        "vis/18/day18",
+    );
+    for i in 0..data.len() {
+        let s = solve(data, i, 70, 70);
+        #[cfg(feature = "vis")]
+        if let Some((_, path)) = &s {
+            let mut g = vec![vec!['.'; 71]; 71];
+            for p in data.iter().take(i) {
+                g.set_value(*p, '#');
+            }
+            for p in path {
+                g.set_value(*p, '*');
+            }
+            gd.draw(&g);
+        }
+        if s.is_none() {
+            println!("{},{}", data[i - 1][0], data[i - 1][1]);
+            break;
+        }
+    }
+    0
+}
+
+fn parse(lines: &[String]) -> Parsed {
+    lines
+        .iter()
+        .map(|x| {
+            let p = aoc::split_ch(x, ',');
+            [p[0].parse().unwrap(), p[1].parse().unwrap()]
+        })
+        .collect()
+}
+
+fn main() {
+    aoc::run_main(parse, part1, part2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn example() -> Vec<String> {
+        let s = include_str!("example.txt");
+        s.lines().map(|x| x.to_string()).collect()
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(solve(&parse(&example()), 12, 6, 6), 22);
+    }
+}

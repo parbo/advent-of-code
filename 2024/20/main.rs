4c6ea97d4c67d09600dea8818159f9908359b947
Pär Bohrarper <par.bohrarper@gmail.com>
2024-12-20 08:32:30 +0100

    wip

--- 2024/20/main.rs
+++ 2024/20/main.rs
@@ -1,57 +1,35 @@
 use std::{cmp::Reverse, collections::BinaryHeap, iter::*};
 
-use aoc::{Grid, GridDrawer};
+use aoc::{Grid, GridDrawer, PrintGridDrawer};
 
 type Parsed = Vec<Vec<char>>;
 
-fn solve(data: &Parsed, threshold: i64) -> i64 {
-    let s = data
-        .points()
-        .find(|p| data.get_value(*p) == Some('S'))
-        .unwrap();
-    let e = data
-        .points()
-        .find(|p| data.get_value(*p) == Some('E'))
-        .unwrap();
-    let (min, max) = data.extents();
-
-    let mut gd = aoc::PrintGridDrawer::new(|c| c);
-    let uncheated_res =
-        aoc::dijkstra_grid(data, |_p, c| *c != '#', |_p1, _c1, _p2, _c2| Some(1), s, e).unwrap();
-    let mut num = 0;
-    for p in data.points() {
-        if p[0] == min[0] || p[0] == max[0] || p[1] == min[1] || p[1] == max[1] {
-            continue;
-        }
-        if let Some('#') = data.get_value(p) {
-            let mut g = data.clone();
-            g.set_value(p, 'c');
-            if let Some(res) =
-                aoc::dijkstra_grid(&g, |_p, c| *c != '#', |_p1, _c1, _p2, _c2| Some(1), s, e)
-            {
-                if res.0 < uncheated_res.0 {
-                    // dbg!(uncheated_res.0 - res.0);
-                    // for pp in res.1 {
-                    //     if pp != p {
-                    //         g.set_value(pp, '*');
-                    //     }
-                    // }
-                    // gd.draw(&g);
-                    if uncheated_res.0 - res.0 >= 100 {
-                        num += 1;
+fn get_neighbors(data: &Parsed, p: aoc::Point, d: i64) -> Vec<(aoc::Point, i64)> {
+    let mut res = vec![];
+    let mut todo = vec![(p, d)];
+    let mut seen = aoc::FxHashSet::default();
+    while let Some((p, d)) = todo.pop() {
+        for nb in aoc::neighbors(p) {
+            if seen.insert(nb) {
+                match data.get_value(nb) {
+                    Some('.') | Some('E') => {
+                        res.push((nb, d));
                     }
+                    Some('#') => {
+                        if d > 0 {
+                            todo.push((nb, d - 1));
+                        }
+                    }
+                    _ => {}
                 }
             }
         }
     }
-    num
+    // dbg!(&res);
+    res
 }
 
-fn part1(data: &Parsed) -> i64 {
-    solve(data, 100)
-}
-
-fn part2(data: &Parsed) -> i64 {
+fn solve(data: &Parsed, threshold: i64, cheats: i64) -> i64 {
     let s = data
         .points()
         .find(|p| data.get_value(*p) == Some('S'))
@@ -65,34 +43,75 @@ fn part2(data: &Parsed) -> i64 {
             .unwrap()
             .0;
     let mut frontier = BinaryHeap::new();
-    let mut visited: aoc::FxHashSet<(aoc::Point, i64)> = aoc::FxHashSet::default();
-    let mut came_from = aoc::FxHashMap::default();
-    frontier.push(Reverse((0, s, 20)));
+    let mut visited = aoc::FxHashSet::default();
+    // let mut came_from = aoc::FxHashMap::default();
+    frontier.push(Reverse((0, s, cheats, vec![])));
     let mut num = 0;
-    while let Some(Reverse((score, current, rem))) = frontier.pop() {
-        if visited.contains(&(current, rem)) {
+    while let Some(Reverse((score, current, rem, path))) = frontier.pop() {
+        if visited.contains(&(current, rem, path.clone())) {
             continue;
         }
-        if current == e && uncheated_res - score >= 100 {
+        if uncheated_res - score < threshold {
+            continue;
+        }
+        if num >= 31 {
+            println!(
+                "{:?}, {} ,{}, {}, {:?}",
+                current,
+                rem,
+                path.len(),
+                frontier.len(),
+                path,
+            );
+            let mut gd = PrintGridDrawer::new(|c| c);
+            let mut g = data.clone();
+            for p in &path {
+                g.set_value(*p, '*');
+            }
+            gd.draw(&g);
+            println!();
+        }
+        if current == e {
+            // let mut path = vec![e];
+            // let mut curr = (e, rem);
+            // while curr.0 != s {
+            //     curr = came_from[&curr];
+            //     path.push(curr.0)
+            // }
+            let mut gd = PrintGridDrawer::new(|c| c);
+            let mut g = data.clone();
+            for p in &path {
+                g.set_value(*p, '*');
+            }
+            gd.draw(&g);
             num += 1;
             continue;
         }
-        for nb in aoc::neighbors(current) {
-            if let Some(value) = data.get_value(nb) {
-                let r = if value == '#' { rem - 1 } else { rem };
-                if r >= 0 {
-                    if visited.contains(&(nb, r)) {
-                        continue;
-                    }
-                    let new_score = score + 1;
-                    came_from.insert(nb, current);
-                    frontier.push(Reverse((new_score, nb, r)));
-                }
+        for (nb, r) in get_neighbors(data, current, rem) {
+            if path.contains(&nb) {
+                continue;
             }
+            // if visited.contains(&(nb, r, )) {
+            //     continue;
+            // }
+            assert!(r >= 0);
+            let new_score = score + aoc::manhattan(current, nb);
+            // came_from.insert((nb, r), (current, rem));
+            let mut newp = path.clone();
+            newp.push(nb);
+            frontier.push(Reverse((new_score, nb, r, newp)));
         }
-        visited.insert((current, rem));
+        visited.insert((current, rem, path.clone()));
     }
     num
+}
+
+fn part1(data: &Parsed) -> i64 {
+    solve(data, 100, 1)
+}
+
+fn part2(data: &Parsed) -> i64 {
+    solve(data, 100, 19)
 }
 
 fn parse(lines: &[String]) -> Parsed {
@@ -114,6 +133,6 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(solve(&parse(&example()), 0), 44);
+        assert_eq!(solve(&parse(&example()), 1, 1), 44);
     }
 }

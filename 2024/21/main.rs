abf45d2f50a1803158095993c6e8925e48d5e583
Pär Bohrarper <par.bohrarper@gmail.com>
2024-12-21 17:01:51 +0100

    works for example, but not real input

--- 2024/21/main.rs
+++ 2024/21/main.rs
@@ -1,8 +1,4 @@
-use std::{
-    cmp::Reverse,
-    collections::{BinaryHeap, VecDeque},
-    iter::*,
-};
+use std::{cmp::Reverse, collections::BinaryHeap};
 
 use aoc::Itertools;
 
@@ -46,83 +42,39 @@ fn to_dir(c: char) -> aoc::Point {
     }
 }
 
-enum Action {
-    Move(aoc::Point),
-    Activate,
-    Idle,
-}
-
-fn press(c: char) -> Action {
-    match c {
-        'A' => Action::Activate,
-        _ => Action::Move(to_dir(c)),
-    }
-}
-
-fn make_move(
-    state: (aoc::Point, aoc::Point, aoc::Point),
-    c: char,
-) -> Option<((aoc::Point, aoc::Point, aoc::Point), Option<char>)> {
-    let mut new_state = state;
-    // The press moves the first robot's position
-    let action = press(c);
-    // The action applies to the second robot
-    let action = match action {
-        Action::Move(m) => {
-            new_state.0 = aoc::point_add(new_state.0, m);
-            dirpad(new_state.0)?;
-            Action::Idle
-        }
-        Action::Activate => press(dirpad(new_state.0).unwrap()),
-        Action::Idle => panic!(),
-    };
-    // Which applies to the third robot
-    let action = match action {
-        Action::Move(m) => {
-            new_state.1 = aoc::point_add(new_state.1, m);
-            dirpad(new_state.1)?;
-            Action::Idle
-        }
-        Action::Activate => press(dirpad(new_state.1).unwrap()),
-        Action::Idle => {
-            // second robot didn't activate
-            Action::Idle
-        }
-    };
-    // Which applies to the last robot
-    let key = match action {
-        Action::Move(m) => {
-            new_state.2 = aoc::point_add(new_state.2, m);
-            keypad(new_state.2)?;
-            None
-        }
-        Action::Activate => Some(keypad(new_state.2).unwrap()),
-        Action::Idle => {
-            // third robot didn't activate
-            None
-        }
-    };
-    Some((new_state, key))
-}
-
 fn find_kp_moves(wanted_code: &[char]) -> Vec<Vec<char>> {
-    let mut todo = VecDeque::new();
-    todo.push_back(([2, 3], Vec::<char>::new(), Vec::<char>::new()));
+    let mut todo = BinaryHeap::new();
+    todo.push(Reverse((0, [2, 3], Vec::<char>::new(), Vec::<char>::new())));
     let mut seen = aoc::FxHashSet::default();
     let mut result: Vec<Vec<char>> = vec![];
-    while let Some((pos, presses, code)) = todo.pop_front() {
+    // let mut best = None;
+    while let Some(Reverse((_score, pos, presses, code))) = todo.pop() {
         if !code.is_empty() && !wanted_code.starts_with(&code) {
             continue;
         }
-        // if !result.is_empty() && presses.len() > result.last().unwrap().len() {
-        //     break;
-        // }
         if code == wanted_code {
+            // if result.contains(&presses) {
+            //     continue;
+            // }
             println!("{:?}", presses.to_vec().iter().join(""));
             result.push(presses);
-            continue;
+            // if let Some(b) = best {
+            //     if score > b {
+            //         break;
+            //     }
+            // }
+            // best = Some(score);
+            // continue;
+            break;
         }
-        for c in ['A', '<', '^', '>', 'v'] {
+        for c in ['A', '^', '>', 'v', '<'] {
+            match (c, presses.last()) {
+                ('>', Some('<')) => continue,
+                ('<', Some('>')) => continue,
+                ('^', Some('v')) => continue,
+                ('v', Some('^')) => continue,
+                _ => {}
+            }
             let mut new_pos = pos;
             let kpc = match c {
                 'A' => Some(keypad(pos).unwrap()),
@@ -140,8 +92,12 @@ fn find_kp_moves(wanted_code: &[char]) -> Vec<Vec<char>> {
             }
             let mut new_presses = presses.clone();
             new_presses.push(c);
+            let turns = new_presses
+                .windows(2)
+                .filter(|x| x[0] != 'A' && x[1] != 'A' && x[0] != x[1])
+                .count();
             if seen.insert((new_pos, new_presses.clone(), new_code.clone())) {
-                todo.push_back((new_pos, new_presses, new_code));
+                todo.push(Reverse((turns, new_pos, new_presses, new_code)));
             }
         }
     }
@@ -150,29 +106,52 @@ fn find_kp_moves(wanted_code: &[char]) -> Vec<Vec<char>> {
 }
 
 fn find_dir_moves(wanted_code: &[char]) -> Vec<Vec<char>> {
-    let mut todo = VecDeque::new();
-    todo.push_back(([2, 0], Vec::<char>::new(), Vec::<char>::new()));
+    let mut todo = BinaryHeap::new();
+    todo.push(Reverse((0, [2, 0], Vec::<char>::new(), Vec::<char>::new())));
     let mut seen = aoc::FxHashSet::default();
     let mut result: Vec<Vec<char>> = vec![];
-    while let Some((pos, presses, code)) = todo.pop_front() {
+    // let mut best = None;
+    while let Some(Reverse((_score, pos, presses, code))) = todo.pop() {
         if !code.is_empty() && !wanted_code.starts_with(&code) {
             continue;
         }
-        // if !result.is_empty() && presses.len() > result.last().unwrap().len() {
-        //     break;
-        // }
+        // println!(
+        //     "{}, {:?}, {:?}, {:?}",
+        //     score,
+        //     wanted_code.to_vec().iter().join(""),
+        //     code.to_vec().iter().join(""),
+        //     presses.to_vec().iter().join(""),
+        // );
         if code == wanted_code {
+            // if result.contains(&presses) {
+            //     continue;
+            // }
             println!("{:?}", presses.to_vec().iter().join(""));
             result.push(presses);
-            continue;
+            // if let Some(b) = best {
+            //     if score > b {
+            //         break;
+            //     }
+            // }
+            // best = Some(score);
+            // continue;
+            break;
         }
-        for c in ['A', '<', '^', '>', 'v'] {
+        for c in ['A', '>', '^', 'v', '<'] {
+            match (c, presses.last()) {
+                ('>', Some('<')) => continue,
+                ('<', Some('>')) => continue,
+                ('^', Some('v')) => continue,
+                ('v', Some('^')) => continue,
+                _ => {}
+            }
             let mut new_pos = pos;
             let kpc = match c {
                 'A' => Some(dirpad(pos).unwrap()),
                 _ => {
                     new_pos = aoc::point_add(pos, to_dir(c));
                     if dirpad(new_pos).is_none() {
+                        // println!("{} not possible at {:?}", c, pos);
                         continue;
                     }
                     None
@@ -184,8 +163,13 @@ fn find_dir_moves(wanted_code: &[char]) -> Vec<Vec<char>> {
             }
             let mut new_presses = presses.clone();
             new_presses.push(c);
-            if seen.insert((new_pos, new_code.clone())) {
-                todo.push_back((new_pos, new_presses, new_code));
+            let turns = new_presses
+                .windows(2)
+                .filter(|x| x[0] != 'A' && x[1] != 'A' && x[0] != x[1])
+                .count();
+            // println!("{:?}, {}", new_presses.to_vec().iter().join(""), turns);
+            if seen.insert((new_pos, new_presses.clone(), new_code.clone())) {
+                todo.push(Reverse((turns, new_pos, new_presses, new_code)));
             }
         }
     }
@@ -200,19 +184,15 @@ fn find_presses(wanted_code: &[char]) -> Vec<char> {
     for pp in p {
         x.extend(find_dir_moves(&pp));
     }
+    // panic!();
     dbg!(x.len());
     let mut y = vec![];
     for pp in x {
         y.extend(find_dir_moves(&pp));
     }
     dbg!(y.len());
-    let mut z = vec![];
-    for pp in y {
-        z.extend(find_dir_moves(&pp));
-    }
-    dbg!(z.len());
-    z.sort_by_key(|x| x.len());
-    z[0].clone()
+    y.sort_by_key(|x| x.len());
+    y[0].clone()
 }
 
 fn part1(data: &Parsed) -> i64 {
@@ -229,40 +209,6 @@ fn part1(data: &Parsed) -> i64 {
         let p = find_presses(wanted_code);
         dbg!(p.len(), num);
         complexity += p.len() as i64 * num;
-        // let mut todo = BinaryHeap::new();
-        // todo.push(Reverse((
-        //     (wanted_code.len(), 0),
-        //     ([2, 0], [2, 0], [2, 3]),
-        //     Vec::<char>::new(),
-        //     Vec::<char>::new(),
-        // )));
-        // let mut seen = aoc::FxHashSet::default();
-        // while let Some(Reverse((_score, state, presses, code))) = todo.pop() {
-        //     if !code.is_empty() && !wanted_code.starts_with(&code) {
-        //         continue;
-        //     }
-        //     if !code.is_empty() {
-        //         println!("{:?}, {:?}", code, presses);
-        //     }
-        //     if &code == wanted_code {
-        //         dbg!(presses);
-        //         continue;
-        //     }
-        //     for c in ['A', '<', '^', '>', 'v'] {
-        //         if let Some((ns, kpc)) = make_move(state, c) {
-        //             let mut new_code = code.clone();
-        //             if let Some(c) = kpc {
-        //                 new_code.push(c);
-        //             }
-        //             let mut new_presses = presses.clone();
-        //             new_presses.push(c);
-        //             let score = (wanted_code.len() - new_code.len(), new_presses.len());
-        //             if seen.insert((ns, new_presses.clone(), new_code.clone())) {
-        //                 todo.push(Reverse((score, ns, new_presses, new_code)));
-        //             }
-        //         }
-        //     }
-        // }
     }
     complexity
 }

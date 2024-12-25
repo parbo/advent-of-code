fbf2a42ced1930a9868eeb297227b9aa89a7bb39
Pär Bohrarper <par.bohrarper@gmail.com>
2024-12-25 14:04:33 +0100

    Day 24 pt 2 done

--- 2024/24/main.rs
+++ 2024/24/main.rs
@@ -1,9 +1,18 @@
 use aoc::Itertools;
+use std::io::prelude::*;
 use std::{collections::BTreeSet, iter::*};
 
-use aoc::FxHashMap;
-
-#[derive(parse_display::Display, parse_display::FromStr, Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(
+    parse_display::Display,
+    parse_display::FromStr,
+    Debug,
+    Clone,
+    PartialEq,
+    Eq,
+    Hash,
+    PartialOrd,
+    Ord,
+)]
 #[display(style = "UPPERCASE")]
 enum Op {
     And,
@@ -16,24 +25,16 @@ type Parsed = (
     Vec<(String, Op, String, String)>,
 );
 
-fn run(
-    values: &aoc::FxHashMap<String, i64>,
-    bits: usize,
-    gates: &[(String, Op, String, String)],
-) -> Option<i64> {
+fn run(values: &aoc::FxHashMap<String, i64>, gates: &[(String, Op, String, String)]) -> i64 {
     let mut values = values.clone();
     let mut zvals = aoc::FxHashSet::default();
     for (a, _op, b, out) in gates {
         for x in [a, b, out] {
-            if let Some(y) = x.strip_prefix("z") {
-                let num: usize = y.trim_start_matches('0').parse().unwrap_or(0);
-                if num < bits {
-                    zvals.insert(x.to_string());
-                }
+            if let Some(_y) = x.strip_prefix("z") {
+                zvals.insert(x.to_string());
             }
         }
     }
-    //    dbg!(&zvals);
     loop {
         let mut v = values.clone();
         for (a, op, b, out) in gates {
@@ -53,14 +54,9 @@ fn run(
                 }
             }
         }
-        let changed = values != v;
         values = v;
         if zvals.iter().all(|x| values.contains_key(x)) {
             break;
-        }
-        if !changed {
-            // println!("strange");
-            return None;
         }
     }
     let z: BTreeSet<(String, i64)> = values
@@ -71,151 +67,129 @@ fn run(
     for (i, (_k, v)) in z.iter().enumerate() {
         zd |= v << i;
     }
-    Some(zd)
-}
-
-fn add(
-    a: i64,
-    b: i64,
-    total_bits: usize,
-    bits: usize,
-    gates: &[(String, Op, String, String)],
-) -> Option<i64> {
-    let mut values = aoc::FxHashMap::default();
-    let digs = total_bits.checked_ilog10().unwrap_or(0) as usize + 1;
-    //    dbg!(bits, digs);
-    for i in 0..bits {
-        let av = a & (1 << i);
-        let bv = b & (1 << i);
-        let x = format!("x{:0digs$}", i, digs = digs);
-        let y = format!("y{:0digs$}", i, digs = digs);
-        values.insert(x, av >> i);
-        values.insert(y, bv >> i);
-    }
-    // dbg!(&values);
-    run(&values, bits, gates)
-}
-
-fn check_valid(
-    total_bits: usize,
-    from_bits: usize,
-    bits: usize,
-    gates: &[(String, Op, String, String)],
-) -> bool {
-    for a in (1 << from_bits)..(1 << bits) {
-        for b in (1 << from_bits)..(1 << bits) {
-            let Some(v) = add(a, b, total_bits, bits, &gates) else {
-                return false;
-            };
-            // dbg!(a, b, v);
-            if v != (a + b) & ((1 << bits) - 1) {
-                return false;
-            }
-        }
-    }
-    true
+    zd
 }
 
 fn part1(data: &Parsed) -> i64 {
-    //    run(&data.0, data.0.len() / 2, &data.1)
-    0
+    run(&data.0, &data.1)
 }
 
 fn part2(data: &Parsed) -> i64 {
     let n = data.0.len() / 2;
-    let bits = n;
-    let digs = bits.checked_ilog10().unwrap_or(0) as usize + 1;
-
-    let mut gates = data.1.clone();
-
-    let mut swapped = vec![];
-
-    // Fix it bit by bit
-    'outer: for k in 1..bits {
-        if check_valid(bits, k - 1, k, &gates) {
-            println!("k == {} is valid", k);
-            continue;
+    let gates = &data.1;
+    let mut wrong = BTreeSet::new();
+    // z should always be xor:ed
+    for (ix, g) in gates.iter().enumerate() {
+        if g.3.starts_with("z") && g.1 != Op::Xor {
+            // Except for the last
+            if g.3.ends_with(&n.to_string()) {
+                continue;
+            }
+            wrong.insert(ix);
         }
-
-        let mut suspect = BTreeSet::new();
-        for i in k..=k {
-            let z = format!("z{:0digs$}", i, digs = digs);
-
-            dbg!(&z);
-            // Check what's reachable
-            let mut seen = BTreeSet::new();
-            let mut todo = vec![z];
-            while let Some(c) = todo.pop() {
-                for (a, op, b, out) in &gates {
-                    if *out == c {
-                        if seen.insert(a.clone()) {
-                            todo.push(a.clone());
-                        }
-                        if seen.insert(b.clone()) {
-                            todo.push(b.clone());
-                        }
-                    }
+    }
+    // An or should always be from two ands
+    for (_ix, g) in gates.iter().enumerate() {
+        if g.1 == Op::Or {
+            for (ixx, gg) in gates.iter().enumerate() {
+                if (gg.3 == g.0 || gg.3 == g.2) && gg.1 != Op::And {
+                    wrong.insert(ixx);
                 }
             }
-            // for j in 0..=i {
-            //     let x = format!("x{:0digs$}", j, digs = digs);
-            //     let y = format!("y{:0digs$}", j, digs = digs);
-            //     seen.remove(&x);
-            //     seen.remove(&y);
-            // }
-            // if seen
-            //     .iter()
-            //     .any(|x| x.starts_with("x") || x.starts_with("y"))
-            // {
-            //     suspect = suspect
-            //         .union(&seen)
-            //         .filter(|x| !x.starts_with("x") && !x.starts_with("y"))
-            //         .cloned()
-            //         .collect();
-            // }
-            suspect = suspect
-                .union(&seen)
-                .filter(|x| !x.starts_with("x") && !x.starts_with("y"))
-                .cloned()
-                .collect();
         }
-        dbg!(&suspect);
-
-        let mut to_swap = vec![];
-        for (ix, (a, op, b, out)) in gates.iter().enumerate() {
-            if suspect.contains(a) || suspect.contains(b) {
-                to_swap.push(ix);
-            }
-        }
-
-        for i in &to_swap {
-            dbg!(i, &gates[*i], gates.len());
-        }
-
-        for x in to_swap.windows(2) {
-            let mut new_gates = gates.clone();
-            let v = new_gates[x[1]].3.clone();
-            new_gates[x[1]].3 = new_gates[x[0]].3.clone();
-            new_gates[x[0]].3 = v;
-            if check_valid(bits, k - 1, k, &new_gates) {
-                println!("SUCCESS for k: {}", k);
-                swapped.push(gates[x[0]].0.clone());
-                swapped.push(gates[x[0]].2.clone());
-                swapped.push(gates[x[1]].0.clone());
-                swapped.push(gates[x[1]].2.clone());
-                gates = new_gates;
-                continue 'outer;
-            } else {
-                println!(
-                    "swapping {:?} and {:?} did not help",
-                    gates[x[0]], gates[x[1]]
-                );
-            }
-        }
-        panic!()
     }
-    dbg!(swapped);
+    // An xor should always have xor and an or
+    for (_ix, g) in gates.iter().enumerate() {
+        if g.1 == Op::Xor {
+            let mut parents = vec![];
+            for (ixx, gg) in gates.iter().enumerate() {
+                if gg.3 == g.0 {
+                    parents.push((ixx, 'L'));
+                }
+                if gg.3 == g.2 {
+                    parents.push((ixx, 'R'));
+                }
+            }
+            if parents.len() != 2 {
+                continue;
+            }
+            let mut maybe_wrong = BTreeSet::new();
+            if gates[parents[0].0].1 == Op::Xor && gates[parents[1].0].1 != Op::Or {
+                maybe_wrong.insert(parents[1].0);
+            }
+            if gates[parents[0].0].1 == Op::Or && gates[parents[1].0].1 != Op::Xor {
+                maybe_wrong.insert(parents[1].0);
+            }
+            if gates[parents[1].0].1 == Op::Xor && gates[parents[0].0].1 != Op::Or {
+                maybe_wrong.insert(parents[0].0);
+            }
+            if gates[parents[1].0].1 == Op::Or && gates[parents[0].0].1 != Op::Xor {
+                maybe_wrong.insert(parents[0].0);
+            }
+            for ix in maybe_wrong {
+                let gg = &gates[ix];
+                if (gg.0.starts_with("x") && gg.2.starts_with("y"))
+                    || (gg.0.starts_with("y") && gg.2.starts_with("x"))
+                {
+                    // xor:s from x and y is ok
+                } else {
+                    wrong.insert(ix);
+                }
+            }
+        }
+    }
+    // and of x and y should always go to or (except for x00 and y00)
+    let re = regex::Regex::new(r"[xy]0+").unwrap();
+    for (ix, g) in gates.iter().enumerate() {
+        if ((g.0.starts_with("x") && g.2.starts_with("y"))
+            || (g.0.starts_with("y") && g.2.starts_with("x")))
+            && g.1 == Op::And
+        {
+            if re.is_match(&g.0) && re.is_match(&g.2) {
+                continue;
+            }
+            for (_ixx, gg) in gates.iter().enumerate() {
+                if (g.3 == gg.0 || g.3 == gg.2) && gg.1 != Op::Or && !gg.3.starts_with("z") {
+                    wrong.insert(ix);
+                }
+            }
+        }
+    }
+
+    let s = wrong.iter().map(|g| gates[*g].3.clone()).sorted().join(",");
+
+    println!("{}", s);
     0
+}
+
+fn dump_dot(filename: &str, gates: &[(String, Op, String, String)]) {
+    let mut zvals = BTreeSet::new();
+    for (a, _op, b, out) in gates {
+        for x in [a, b, out] {
+            if let Some(_y) = x.strip_prefix("z") {
+                zvals.insert(x.to_string());
+            }
+        }
+    }
+    let mut seen = BTreeSet::new();
+    let mut todo: Vec<_> = zvals.into_iter().collect();
+    let mut dot = vec!["digraph {".to_string()];
+    while let Some(c) = todo.pop() {
+        for g in gates {
+            if g.3 == c && seen.insert(g.clone()) {
+                dot.push(format!("{} -> \"{} {} {}\"", g.0, g.0, g.1, g.2));
+                dot.push(format!("{} -> \"{} {} {}\"", g.2, g.0, g.1, g.2));
+                dot.push(format!("\"{} {} {}\" -> {}", g.0, g.1, g.2, g.3));
+                todo.push(g.0.clone());
+                todo.push(g.2.clone());
+            }
+        }
+    }
+    dot.push("}".to_string());
+    let dot = dot.iter().join("\n");
+    let path = std::path::Path::new(filename);
+    let mut file = std::fs::File::create(&path).unwrap();
+    file.write_all(dot.as_bytes()).unwrap();
 }
 
 fn parse(lines: &[String]) -> Parsed {
@@ -239,30 +213,10 @@ fn parse(lines: &[String]) -> Parsed {
             )
         })
         .collect();
+    dump_dot("graph.dot", &gates);
     (init, gates)
 }
 
 fn main() {
     aoc::run_main(parse, part1, part2);
-}
-
-#[cfg(test)]
-mod tests {
-    // use super::*;
-
-    // fn example() -> Vec<String> {
-    //     let s = include_str!("example.txt");
-    //     s.lines().map(|x| x.to_string()).collect()
-    // }
-
-    // fn example() -> Vec<String> {
-    // 	   vec![
-    //         "0".into()
-    //     ]
-    // }
-
-    // #[test]
-    // fn test_part1() {
-    //     assert_eq!(part1(&parse(&example())), 0);
-    // }
 }

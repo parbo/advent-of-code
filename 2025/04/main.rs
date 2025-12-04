5e956e1abfe3af8af3fb4a8bd0eefb687c17f368
Pär Bohrarper <par.bohrarper@gmail.com>
2025-12-04 06:57:12 +0100

    Add vis

--- 2025/04/main.rs
+++ 2025/04/main.rs
@@ -1,11 +1,14 @@
 use aoc::{Grid, Point};
 
+#[cfg(feature = "vis")]
+use aoc::GridDrawer;
+
 type Parsed = Vec<Vec<char>>;
 
 fn get_removable(g: &Parsed) -> Vec<Point> {
     let mut to_remove = vec![];
     for p in g.points() {
-        if g.get_value(p) == Some('.') {
+        if g.get_value(p) != Some('@') {
             continue;
         }
         let num = aoc::neighbors_incl_diagonals(p)
@@ -25,14 +28,30 @@ fn part1(data: &Parsed) -> i64 {
 fn part2(data: &Parsed) -> i64 {
     let mut ans = 0;
     let mut g = data.clone();
+    #[cfg(feature = "vis")]
+    let mut gd = aoc::make_bitmap_text_grid_drawer(
+        |x| match x {
+            '@' => (x, [0xff, 0xff, 0xff]),
+            'x' => ('@', [0xff, 0, 0]),
+            _ => (x, [0x7f, 0x7f, 0x7f]),
+        },
+        "vis/04/day04",
+    );
     loop {
         let to_remove = get_removable(&g);
         if to_remove.is_empty() {
             break;
         }
-        for p in to_remove {
-            g.set_value(p, '.');
+        for p in &to_remove {
+            g.set_value(*p, 'x');
             ans += 1;
+        }
+        #[cfg(feature = "vis")]
+        {
+            gd.draw(&g);
+        }
+        for p in &to_remove {
+            g.set_value(*p, '.');
         }
     }
     ans

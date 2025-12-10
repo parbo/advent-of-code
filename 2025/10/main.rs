ced91733f9ca2a60e9083c936cb303ca6cb53ca4
Pär Bohrarper <bohrarper@spotify.com>
2025-12-10 15:12:00 +0100

    try z3 solution

--- 2025/10/main.rs
+++ 2025/10/main.rs
@@ -1,10 +1,8 @@
-use std::{
-    cmp::Reverse,
-    collections::{BinaryHeap, VecDeque},
-    iter::*,
+use std::collections::VecDeque;
+use z3::{
+    ast::{Array, Int},
+    with_z3_config, Config, Optimize, SatResult, Sort,
 };
-
-use aoc::FxHashSet;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Machine {
@@ -38,48 +36,79 @@ fn part1(data: &Parsed) -> i64 {
 }
 
 fn part2(data: &Parsed) -> i64 {
-    let mut sum = 0;
+    let mut tot_sum = 0;
     for m in data {
-        let mut todo = BinaryHeap::new();
-        let mut seen = FxHashSet::default();
-        let lcm = aoc::lcm_arr(&m.joltages);
-        dbg!(lcm);
-        let len = m.joltages.len();
-        let v = vec![0; len];
-        todo.push(Reverse((m.joltages.iter().sum(), 0, v)));
-        let mut res = -1;
-        let mut last_num = 0;
-        'outer: while let Some(Reverse((_d, num, state))) = todo.pop() {
-            // println!("{:?} {:?}, {}", state, m.joltages, num);
-            if state == m.joltages {
-                // println!("{:?} {:?}, {}", state, m.joltages, num);
-                if last_num > 0 && num > last_num {
-                    break;
-                }
-                last_num = num;
-                res = num;
-            }
-            for b in &m.buttons {
-                let mut new_state = state.clone();
-                let mut d = 0;
-                for i in 0..len {
+        // Set up Z3 globally via a thread-local context
+        let cfg = Config::new();
+
+        with_z3_config(&cfg, || {
+            let opt = Optimize::new();
+
+            let int_sort = Sort::int();
+            let tots = Array::new_const("tots", &int_sort, &int_sort);
+            let num_presses = Array::new_const("num_presses", &int_sort, &int_sort);
+
+            for (i, joltage) in m.joltages.iter().enumerate() {
+                let idx = Int::from_i64(i as i64);
+                let mut tot = tots.select(&idx).as_int().unwrap();
+                // For each button that contributes to this joltage, add its button presses
+                for (k, b) in m.buttons.iter().enumerate() {
                     if *b & (1 << i) != 0 {
-                        new_state[i] += 1;
+                        let bdx = Int::from_i64(k as i64);
+                        let pval = num_presses.select(&bdx).as_int().unwrap();
+                        tot += pval;
                     }
-                    if new_state[i] > m.joltages[i] {
-                        continue 'outer;
-                    }
-                    d += m.joltages[i] - new_state[i];
                 }
-                if seen.insert((num + 1, new_state.clone())) {
-                    todo.push(Reverse((d, num + 1, new_state)));
+                opt.assert(&tot.eq(*joltage));
+            }
+            let mut sum = Int::from_i64(0);
+            for (k, _b) in m.buttons.iter().enumerate() {
+                let bdx = Int::from_i64(k as i64);
+                let pval = num_presses.select(&bdx).as_int().unwrap();
+                opt.assert(&pval.ge(0));
+                sum += pval;
+            }
+
+            println!("{opt:?}");
+
+            // Minimize the sum of the array entries
+            opt.minimize(&sum);
+
+            match opt.check(&[]) {
+                SatResult::Sat => {
+                    let model = opt.get_model().unwrap();
+
+                    println!("{model:?}");
+
+                    println!("==========================");
+                    for i in 0..m.buttons.len() {
+                        let idx = Int::from_i64(i as i64);
+                        let val = num_presses.select(&idx).as_int().unwrap();
+                        let v = model.eval(&val, true).unwrap();
+                        println!("num_presses[{}] = {}", i, v);
+                    }
+                    for i in 0..m.joltages.len() {
+                        let idx = Int::from_i64(i as i64);
+                        let tval = tots.select(&idx).as_int().unwrap();
+                        let tv = model.eval(&tval, true).unwrap();
+                        println!("tots[{}] = {}", i, tv);
+                    }
+
+                    let sum_val = model.eval(&sum, true).unwrap();
+                    tot_sum += sum_val.as_i64().unwrap();
+                }
+                SatResult::Unsat => {
+                    println!("unsat");
+                    panic!();
+                }
+                SatResult::Unknown => {
+                    println!("unknown: {:?}", opt.get_reason_unknown());
+                    panic!();
                 }
             }
-        }
-        dbg!(res);
-        sum += res;
+        });
     }
-    sum
+    tot_sum
 }
 
 fn parse(lines: &[String]) -> Parsed {
